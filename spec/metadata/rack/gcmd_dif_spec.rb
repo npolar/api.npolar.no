5eef4af84677af11ff7ee778b9b53866464406c5
RDux <ruben.dens@gmail.com>
2012-05-03 10:59:02 +0200

    Added Metadata Middleware Spec

---
+++ spec/metadata/rack/gcmd_dif_spec.rb
@@ -0,0 +1,42 @@
+require_relative "../../spec_helper"
+require_relative "../../../lib/metadata/rack/gcmd_dif"
+
+describe Metadata::Rack::GcmdDif do
+  
+  before do
+    body = '{"Entry_Title": "my title", "Entry_ID": "myID", "Summary": {"Abstract":"my summary"}}'
+    @app = lambda { |env| [200, { "Content-Type" => "application/json"}, [body]] }
+  end
+  
+  context "when receiving a GET with dif or xml format" do
+    
+    it "should return DIF xml when it receives a request with the .xml extension" do
+      request = Rack::MockRequest.env_for("/mydif.xml")
+      body = Metadata::Rack::GcmdDif.new(@app).call(request).last      
+      body.first.should include("<Entry_ID>myID</Entry_ID>", "<Entry_Title>my title</Entry_Title")
+    end
+    
+    it "should return DIF xml when it receives a request with the .dif extension" do
+      request = Rack::MockRequest.env_for("/mydif.dif")
+      body = Metadata::Rack::GcmdDif.new(@app).call(request).last
+      body.first.should include("<Entry_ID>myID</Entry_ID>", "<Entry_Title>my title</Entry_Title", "<Abstract>my summary</Abstract>")
+    end
+    
+  end
+  
+  context "when receiving a request with any format execept (.dif|.xml) or no format" do
+    
+    it "should return json with no extension" do
+      request = Rack::MockRequest.env_for("/mydif")
+      body = Metadata::Rack::GcmdDif.new(@app).call(request).last
+      body.first.should include('"Entry_ID": "myID"')
+    end
+    
+    it "should return json with a random extension" do
+      request = Rack::MockRequest.env_for("/mydif.conrad")
+      body = Metadata::Rack::GcmdDif.new(@app).call(request).last
+      body.first.should include('"Entry_Title": "my title"')
+    end
+    
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,10 @@
+require "bundler/setup"
+require "rspec"
+require "rack/test"
+
+# Test environment
+ENV["RACK_ENV"] = "test"
+
+RSpec.configure do |conf|
+  conf.include Rack::Test::Methods
+end
