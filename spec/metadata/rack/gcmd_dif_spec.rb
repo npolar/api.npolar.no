b0947de41ac7c4ac460e08826a0e71ed5417807f
RDux <ruben.dens@gmail.com>
2012-05-04 11:52:22 +0200

    Added additional tests for json format and no format requests

--- spec/metadata/rack/gcmd_dif_spec.rb
+++ spec/metadata/rack/gcmd_dif_spec.rb
@@ -11,7 +11,7 @@ describe Metadata::Rack::GcmdDif do
     @default_body = Metadata::Rack::GcmdDif.new(@app).call(default_request).last
   end
     
-  context "when receiving a GET with dif or xml format" do
+  context "when receiving a GET" do
     
     it "should require a valid id" do
       invalid_request = Rack::MockRequest.env_for("/.xml")      
@@ -31,28 +31,51 @@ describe Metadata::Rack::GcmdDif do
       body.first.should include("<Entry_ID>myID</Entry_ID>", "<Entry_Title>my title</Entry_Title", "<Abstract>my summary</Abstract>")
     end
     
-    it "should return Content-Type: application/xml in the headers" do
+    it "should return json when getting a request with the .json format" do
+      request = Rack::MockRequest.env_for("/id.json")
+      body = Metadata::Rack::GcmdDif.new(@app).call(request).last
+      body.first.should include('"Entry_ID": "myID"')
+    end
+    
+    it "should return json on a request with no format specified" do
+      request = Rack::MockRequest.env_for("/mydif")
+      body = Metadata::Rack::GcmdDif.new(@app).call(request).last
+      body.first.should include('"Entry_ID": "myID"')
+    end
+    
+    it "should return correct headers for requests with the .xml format" do
       request = Rack::MockRequest.env_for("/id.xml")
       status, headers, body = Metadata::Rack::GcmdDif.new(@app).call(request)
       headers.should include("Content-Type"=>"application/xml")
     end
     
+    it "should return correct headers for requests with the .dif format" do
+      request = Rack::MockRequest.env_for("/id.dif")
+      status, headers, body = Metadata::Rack::GcmdDif.new(@app).call(request)
+      headers.should include("Content-Type"=>"application/xml")
+    end
+    
+    it "should return correct headers for requests with the .json format" do
+      request = Rack::MockRequest.env_for("/mydif.json")
+      status, headers, body = Metadata::Rack::GcmdDif.new(@app).call(request)
+      headers.should include("Content-Type"=>"application/json")
+    end
+    
+    it "should return correct headers for requests with no format" do
+      request = Rack::MockRequest.env_for("/mydif")
+      status, headers, body = Metadata::Rack::GcmdDif.new(@app).call(request)
+      headers.should include("Content-Type"=>"application/json")
+    end
+    
     it "should return the correct Content-Length" do     
       request = Rack::MockRequest.env_for("/id.xml")
-      status, headers, body = Metadata::Rack::GcmdDif.new(@app).call(request)
-      
+      status, headers, body = Metadata::Rack::GcmdDif.new(@app).call(request)      
       headers.should include("Content-Length"=>"#{body.first.size.to_s}")
-    end
+    end   
     
   end
   
-  context "when receiving a GET request with any format execept (.dif|.xml) or no format" do
-    
-    it "should return json on a request with no extension" do
-      request = Rack::MockRequest.env_for("/mydif")
-      body = Metadata::Rack::GcmdDif.new(@app).call(request).last
-      body.first.should include('"Entry_ID": "myID"')
-    end
+  context "when receiving a GET request with any format execept (.dif|.xml|.json|no format)" do    
     
     it "should return 406 (Not acceptable) with a random extension" do
       request = Rack::MockRequest.env_for("/mydif.sagw")
@@ -60,12 +83,18 @@ describe Metadata::Rack::GcmdDif do
       status.should == 406
     end
     
+    it "should return json in the body" do
+      request = Rack::MockRequest.env_for("/mydif.sagw")
+      body = Metadata::Rack::GcmdDif.new(@app).call(request).last
+      body.first.should include('"Entry_ID": "myID"')
+    end    
+    
     it "should return Content-Type: application/json in the headers" do
-      request = Rack::MockRequest.env_for("/mydif")
+      request = Rack::MockRequest.env_for("/mydif.sagw")
       status, headers, body = Metadata::Rack::GcmdDif.new(@app).call(request)
       headers.should include("Content-Type"=>"application/json")
     end
     
   end
-  
+    
 end
