b397d8efc764e2c81b44c0b96998e9a5fe945303
RDux <ruben.dens@gmail.com>
2012-05-09 10:49:31 +0200

    Added Metadata middleware that intercepts incoming requests with xml and converts that xml to json before passing the modified request to the server

---
+++ lib/metadata/rack/save_gcmd_dif.rb
@@ -0,0 +1,44 @@
+require "gcmd/dif"
+require "yajl/json_gem"
+
+module Metadata
+  module Rack
+    class SaveGcmdDif
+      
+      def initialize(app)
+        @app = app
+      end
+
+      def call(env)
+                
+        request = ::Rack::Request.new(env)
+        puts @app.inspect
+                
+        if request.put? || request.post?
+          
+          if request.env["CONTENT_TYPE"] == "application/xml"
+          
+            body = request.body.read
+            
+            dif = Gcmd::Dif.new()
+            json = dif.load_xml( body )
+            
+            json = json.to_json.to_s
+            
+            env["CONTENT_TYPE"] = "application/json"
+            env["CONTENT_LENGTH"] = (json.size + 1).to_s
+            
+            input = ::Rack::Lint::InputWrapper.new( StringIO.new( json ) )
+            
+            env["rack.input"] = input            
+            
+          end          
+          
+        end
+        
+        @app.call(env)
+        
+      end      
+    end
+  end
+end
