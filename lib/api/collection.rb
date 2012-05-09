a3bbd1bcad7febed0f08e400040271a583a698dd
RDux <ruben.dens@gmail.com>
2012-05-09 10:50:33 +0200

    Removed obsolete methods that have been replaced by the Metadata middlewares

--- lib/api/collection.rb
+++ lib/api/collection.rb
@@ -1,128 +1,72 @@
 module Api
-class Collection
-
-  attr_accessor :validators
-
-  def initialize(depot)
-    @depot = depot
-    @preprocessors = []
-    @validators = []
-    @postprocessors = []
-  end
-
-  def add_validator(validator)
-    @validators << validator
-  end
-
-  def delete(id, headers = {})
-
-    # Set id and headers so that these could be processed by #(before|after)_delete
-    @id = id
-    @headers = headers
-    
-    before_delete
-    @response = @depot.delete(@id, @headers)
-    after_delete
-            
-    @response
-    
-  end
-
-  def get(id, headers = {})
-    before_request("GET", id, headers)
-    
-    before_read
-    @response = @depot.get(@id, @headers)
-    after_read
-
-    @response
-  end
-
-  def head(id, headers = {})
-    before_request("HEAD", id, headers)
-    
-    before_read
-    @response = @depot.head(@id, @headers)
-    after_read
-    
-    @response
-  end
-
-  def options(id, headers = {})
-    # Set id and headers so that these could be processed by #(before|after)_read
-    @id = id
-    @headers = headers
-    
-    before_read
-    @response = @depot.options(@id, @headers)
-    after_read
-    
-    @response
-  end
+  class Collection
   
-  def search
-    @depot.feed
-  end
-
-  def trace(id, headers = {})
-    # Set id and headers so that these could be processed by #(before|after)_read
-    @id = id
-    @headers = headers
-    
-    before_read
-    @response = @depot.trace(@id, @headers)
-    after_read
-    
-    @response
-  end
-
-
-  def put(id, data, headers = {})
+    attr_accessor :validators
+  
+    def initialize(depot)
+      @depot = depot
+      @preprocessors = []
+      @validators = []
+      @postprocessors = []
+    end
+  
+    def add_validator(validator)
+      @validators << validator
+    end
+  
+    def delete(id, headers = {})
+      @response = @depot.delete(id, headers)              
+      @response
       
-    # Set id, data and headers so that these could be processed by #(before|after)_save
-    @id = id
-    @data = data
-    @headers = headers
-    
-    before_save 
-    @response = @depot.put(@id, @data)
-    after_save
-    
-    @response
-  end
+    end
   
-  def post(data, headers = {})
-    raise "Not implemented"
-  end
+    def get(id, headers = {})
+      before_request("GET", id, headers)
 
-
-  protected
-
-  def after_read
-  end
-
-  def after_save
-  end
-
-  def after_delete
-  end
-
-  def before_request(method, id, headers)
-    # Set @id, @format and @headers so that these could be processed by #(before|after)_read
-    @id, @format = id.split(".")
-    @headers = headers
-  end
-
-  def before_read
-      
-  end
+      @response = @depot.get(@id, @headers)  
+      @response
+    end
   
-  def before_save
-    # add timestamp
-  end
+    def head(id, headers = {})
+      before_request("HEAD", id, headers)
 
-  def before_delete
-  end
+      @response = @depot.head(@id, @headers)
+      @response
+    end
+  
+    def options(id, headers = {})
+      @response = @depot.options(id, headers)
+      @response
+    end
+    
+    def search
+      @depot.feed
+    end
+  
+    def trace(id, headers = {})
+      @response = @depot.trace(id, headers)
+      @response
+    end
+  
+  
+    def put(id, data, headers = {})  
+      @response = @depot.put(id, data)      
+      @response
+    end
+    
+    def post(data, headers = {})
+      @response = @depot.post(id, data)      
+      @response
+    end
+  
+  
+    protected
+  
+    def before_request(method, id, headers)
+      # Set @id, @format and @headers so that these could be processed by #(before|after)_read
+      @id, @format = id.split(".") unless id == ""
+      @headers = headers
+    end
 
   end
 end
