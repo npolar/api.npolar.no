3833379e75158d218fd320a5075cf896705a8122
RDux <ruben.dens@gmail.com>
2012-05-09 10:46:10 +0200

    added head method and fixed Content-Type header for feed method

--- lib/api/storage/couch.rb
+++ lib/api/storage/couch.rb
@@ -20,11 +20,15 @@ module Api
       def get(id, headers={})
         super
       end
+            
+      def head(id, headers={})
+        super
+      end
       
       def put(id, data, headers={})
         
         status, headers, body = super
-        
+                
         # if successful create, return the saved document
         if 201 == status
           r = headers["Etag"].gsub(/["]/, "")
@@ -33,10 +37,10 @@ module Api
         
         [status, headers, body]
       end
-
+      
       def feed(params={}, headers={})
 
-        puts @http.inspect
+        #puts @http.inspect
         response = @http.get("_all_docs?include_docs=true")
 
         result= JSON.parse(response.body)
@@ -54,8 +58,8 @@ module Api
 
         header = Api::Atom::Feed.header
         header["opensearch:totalResults"] = entries.size
-
-        [200, {}, { "feed" => { "header" => header, "entry" => entries } }.to_json]
+        
+        [200, {"Content-Type"=>"application/json"}, { "feed" => { "header" => header, "entry" => entries } }.to_json]
         # 304 403 200
       end
       
