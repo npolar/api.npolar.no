89f1779d03be3512d3db999fdef338d246d15f28
RDux <ruben.dens@gmail.com>
2012-05-10 12:57:18 +0200

    Added import support for OAI format

--- lib/metadata/rack/save_gcmd_dif.rb
+++ lib/metadata/rack/save_gcmd_dif.rb
@@ -13,29 +13,43 @@ module Metadata
                 
         request = ::Rack::Request.new(env)
          
-        if request.put? || request.post?
-          
+        if request.put? || request.post?          
           if request.env["CONTENT_TYPE"] == "application/xml"
           
             body = request.body.read
             
             dif = Gcmd::Dif.new()
             json = dif.load_xml( body )
+
+            if json.class == Array
+              json.each do | document |                
+                build_request( env, document )                 
+              end              
+              json = nil
+            end            
             
-            json = json.to_json.to_s
+            build_request( env, json )
             
-            env["CONTENT_TYPE"] = "application/json"
-            env["CONTENT_LENGTH"] = json.bytesize.to_s
-            
-            input = ::Rack::Lint::InputWrapper.new( StringIO.new( json ) )
-            
-            env["rack.input"] = input            
-            
-          end          
-          
+          end      
         end
         
-        @app.call(env)        
+        @app.call(env)
+        
+      end
+      
+      def build_request( env, body )
+        
+        unless body.nil?
+          json = body.to_json.to_s
+            
+          env["CONTENT_TYPE"] = "application/json"
+          env["CONTENT_LENGTH"] = json.bytesize.to_s          
+          env["rack.input"] = ::Rack::Lint::InputWrapper.new( StringIO.new( json ) )
+          
+          @app.call(env)
+        else
+          [201, {"Content-Type" => "text/html"}, ["OAI import succesful"]]
+        end
         
       end      
     end
