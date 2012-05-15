6757354f880bb059d9c2ef7ab766dc5c95c4daea
RDux <ruben.dens@gmail.com>
2012-05-15 11:02:22 +0200

    fixed feed message and header creation

--- lib/api/storage/couch.rb
+++ lib/api/storage/couch.rb
@@ -24,15 +24,16 @@ module Api
       def head(id, headers={})
         super
       end
-      
+        
       def put(id, data, headers={})
         
         status, headers, body = super
                 
         # if successful create, return the saved document
         if 201 == status
-          r = headers["Etag"].gsub(/["]/, "")
-          body = get(id, { :rev => r })[2]
+          r = headers["Etag"].gsub(/["]/, "") # Get the revision number from the Etag header
+          body = get(id, { :rev => r })[2] # Request content for the specific revision
+          headers["Content-Length"] = body.bytesize.to_s # Set correct Content-Length
         end
         
         [status, headers, body]
@@ -40,13 +41,10 @@ module Api
       
       def feed(params={}, headers={})
 
-        #puts @http.inspect
         response = @http.get("_all_docs?include_docs=true")
-
-        result= JSON.parse(response.body)
+        result = JSON.parse(response.body)
 
         # use Api::Atom::Feed
-        feed = {}
         entries = result["rows"].map {|e|e["doc"]}
 
         # on save
@@ -55,11 +53,19 @@ module Api
           entry.delete "_id"
           entry["link"] = { "href" => entry["code"], "rel" => "edit"} # if not exists?
         end
-
-        header = Api::Atom::Feed.header
-        header["opensearch:totalResults"] = entries.size
         
-        [200, {"Content-Type"=>"application/json"}, { "feed" => { "header" => header, "entry" => entries } }.to_json]
+        data_header = Api::Atom::Feed.header
+        data_header["opensearch:totalResults"] = entries.size
+        
+        feed = {}
+        feed["header"] = data_header
+        feed["entry"] = entries
+
+        headers = response.headers
+        headers["Content-Type"] = "application/json"
+        headers["Content-Lenght"] = feed.to_json.bytesize.to_s
+        
+        [200, headers, feed.to_json]
         # 304 403 200
       end
       
