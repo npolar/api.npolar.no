2dd5061a1325257d6bd24f7a143c4a2b49ec1b47
cnrdh <conrad.helgeland@gmail.com>
2012-06-11 16:50:12 +0200

    Protecting against putting/deleting without id. Getting rid of instance variables

--- lib/api/collection.rb
+++ lib/api/collection.rb
@@ -1,15 +1,20 @@
 module Api
+
+  # http://stackoverflow.com/questions/5513558/executing-code-for-every-method-call-in-a-ruby-module
+# status if >= 400 rewrite response to standard format
   class Collection
 
-    attr_accessor :validators, :formats, :format, :storage
+    attr_accessor :validators, :formats, :format, :storage, :accepts, :response
 
-    # http://stackoverflow.com/questions/5513558/executing-code-for-every-method-call-in-a-ruby-module
+    ACCEPTS = ["json"]
+
     FORMATS = ["json"]
 
     FORMAT = "json"
 
     def initialize(storage=nil)
       @storage = storage
+      @accepts = ACCEPTS
       @formats = FORMATS
       @format = FORMAT
       @preprocessors = []
@@ -18,36 +23,41 @@ module Api
     end
 
     def add_validator(validator)
-      @validators << validator
+      validators << validator
     end
 
     def delete(id, headers = {})
-      @response = @storage.delete(id, headers)
+      # Never EVER delete without an id
+      if id.nil? or id.empty? or id =~ /\s+/
+        raise "Cannot delete a resource when id is blank"
+      end
+      response = storage.delete(id, headers)
     end
 
     def get(id, headers = {})
-      @response = @storage.get(id, headers)
+      response = storage.get(id, headers)
     end
 
     def head(id, headers = {})
-      @response = @storage.head(@id, @headers)
-    end
-
-    def options(id, headers = {})
-      @response = @storage.options(id, headers)
+      response = storage.head(id, headers)
     end
 
     def search
-      @response = @storage.feed
-    end
-
-    def put(id, data, headers = {})
-      @response = @storage.put(id, data, headers)
+      response = storage.feed
     end
 
     def post(data, headers = {})
-      @response = @storage.post(data, headers)
+      response = storage.post(data, headers)
     end
+
+    def put(id, data, headers = {})
+      # Never EVER put without an id
+      if id.nil? or id.empty? or id =~ /\s+/
+        raise "Cannot put data when id is blank"
+      end
+      response = storage.put(id, data, headers)
+    end
+
 
   end
 end
