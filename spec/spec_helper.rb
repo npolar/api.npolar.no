886ebf3f1e403054be93cd1a17ef20295d04ccaa
cnrdh <conrad.helgeland@gmail.com>
2012-06-11 16:52:10 +0200

    Added SimpleCov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,15 @@
+require 'simplecov'
+SimpleCov.start do
+  add_filter "/rackup/"
+  add_filter "/spec/"
+  add_group "Server", "lib/api/server"
+  add_group "Collection", "lib/api/collection"
+  add_group "Storage", "lib/api/storage"
+  add_group "Rack", "lib/api/rack"
+  
+
+end
+
 require "bundler/setup"
 require "rspec"
 require "rack/test"
