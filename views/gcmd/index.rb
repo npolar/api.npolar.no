b269cdd26fda128f94bf50df2739534f769b5d99
cnrdh <conrad.helgeland@gmail.com>
2012-07-23 16:32:08 +0200

    JSONP service for GCMD Concepts

--- views/gcmd/index.rb
+++ views/gcmd/index.rb
@@ -1,4 +1,4 @@
-class Gcmd::Index < Mustache
+class Gcmd::Index < Html
 
   self.template_path = File.expand_path(File.dirname(__FILE__)+"/..")
 
@@ -10,12 +10,26 @@ class Gcmd::Index < Mustache
     Gcmd::Concepts::ROOT_SCHEMES.map {|scheme| { :scheme => scheme, :path => path(scheme) } }
   end
 
-  def credits
-    "GCMD credit"
+  def credits_html
+    "<h4>Credits</h4>
+<p>Data provided by #{link("http://gcmd.nasa.gov", "GCMD")}'s #{link("http://gcmdservices.gsfc.nasa.gov/kms/", "Keyword Management System")}</p>"
   end
 
   def path(scheme)
     "/gcmd/#{scheme}"
+  end
+
+  def link(href, title=nil)
+    title = title.nil? ? href : title
+    "<a href=\"#{href}\">#{title}</a>"
+  end
+
+def request
+  "GET #{link("/gcmd/locations?q=arctic")}"
+end
+
+  def response
+    [["d40d9651-aa19-4b2c-9764-7371bb64b9a7","ARCTIC"],["70fb5a3b-35b1-4048-a8be-56a0d865281c","ANTARCTICA"],["1ed45273-3e2b-4586-b852-05578c04041b","ARCTIC OCEAN"]].to_json
   end
 
 end
