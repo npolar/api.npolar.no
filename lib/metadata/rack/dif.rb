19b53874df523da7b4dd00f1b2985deecdecf2e1
RDux <ruben.dens@gmail.com>
2012-08-10 15:03:50 +0200

    Fixed Indentation

--- lib/metadata/rack/dif.rb
+++ lib/metadata/rack/dif.rb
@@ -7,152 +7,150 @@ module Metadata
 
   module Rack
 
-      class Dif < Npolar::Rack::Middleware
+    class Dif < Npolar::Rack::Middleware
 
-        FORMATS = ["atom", "dif", "iso", "xml"]
+      FORMATS = ["atom", "dif", "iso", "xml"]
 
-        ACCEPTS = ["xml", "dif"]
+      ACCEPTS = ["xml", "dif"]
 
-        XML_HEADER_HASH = {"Content-Type" => "application/xml; charset=utf-8"}
+      XML_HEADER_HASH = {"Content-Type" => "application/xml; charset=utf-8"}
 
-        JSON_HEADER_HASH = {"Content-Type" => "application/json; charset=utf-8"}
-        
-        XSL = "DIF-ISO-3.1.xsl"
+      JSON_HEADER_HASH = {"Content-Type" => "application/json; charset=utf-8"}
+      
+      XSL = "DIF-ISO-3.1.xsl"
 
-        def condition? request
-          # GET
-          if (FORMATS.include? request.format or request.content_type =~ /application\/xml/) and "GET" == request.request_method
-            true
-          # POST, PUT
-          elsif (ACCEPTS.include? request.format or request.content_type =~ /application\/xml/) and ["POST","PUT"].include? request.request_method
-            true
+      def condition? request
+        # GET
+        if (FORMATS.include? request.format or request.content_type =~ /application\/xml/) and "GET" == request.request_method
+          true
+        # POST, PUT
+        elsif (ACCEPTS.include? request.format or request.content_type =~ /application\/xml/) and ["POST","PUT"].include? request.request_method
+          true
+        else
+          false
+        end
+      end
+
+      # Only called if #condition? is true
+      def handle(request)
+        case request.request_method
+          when "GET" then dif_from_json(request)
+          when "POST", "PUT" then dif_save(request)
+        end
+      end
+
+      def dif_save(request)
+        xml = request.body.read
+        builder = ::Gcmd::HashBuilder.new( xml )
+        difs = builder.build_hash_documents
+        j = []
+        difs.each do | dif_hash |
+          dif_atom = ::Metadata::DifAtom.new
+          atom_hash = dif_atom.atom_from_dif(dif_hash)
+          j << atom_hash
+        end
+        if 1 == j.size
+          j = j[0]
+        end
+
+        json = j.to_json
+
+        request.env["CONTENT_TYPE"] = "application/json"
+        request.env["CONTENT_LENGTH"] = json.bytesize.to_s
+        request.env["rack.input"] = ::Rack::Lint::InputWrapper.new( StringIO.new( json ) )
+        app.call(request.env)
+      end
+
+      def dif_from_json(request)
+
+        response = app.call(request.env)
+
+        if response.status < 300
+
+          metadata_dataset = ::Yajl::Parser.parse(response.io)
+
+          # if id false? and many => pack xml in somethimg p metadata_dataset
+
+          xml = case request.format
+            when "dif", "xml"
+              then dif_xml(dif_json(metadata_dataset))
+            when "atom"
+              then atom_entry(metadata_dataset).to_xml
+            when "iso"
+              then iso(dif_xml(dif_json(metadata_dataset)))
+          end
+
+          if "validate" == request.path_info.split("/").last
+            dif = ::Gcmd::Dif.new
+            dif.load_xml xml
+            report = dif.validate_xml
+            [200, JSON_HEADER_HASH, [report.to_json]]
           else
-            false
-          end
-        end
-
-        # Only called if #condition? is true
-        def handle(request)
-          case request.request_method
-            when "GET" then dif_from_json(request)
-            when "POST", "PUT" then dif_save(request)
-          end
-        end
-
-        def dif_save(request)
-          xml = request.body.read
-          builder = ::Gcmd::HashBuilder.new( xml )
-          difs = builder.build_hash_documents
-          j = []
-          difs.each do | dif_hash |
-            dif_atom = ::Metadata::DifAtom.new
-            atom_hash = dif_atom.atom_from_dif(dif_hash)
-            j << atom_hash
-          end
-          if 1 == j.size
-            j = j[0]
+            [200, XML_HEADER_HASH, [xml]]
           end
 
-          json = j.to_json
-
-          request.env["CONTENT_TYPE"] = "application/json"
-          request.env["CONTENT_LENGTH"] = json.bytesize.to_s  
-          request.env["rack.input"] = ::Rack::Lint::InputWrapper.new( StringIO.new( json ) )
-          app.call(request.env)
 
 
+        else
+          response
         end
+      end
 
-        def dif_from_json(request)
+      protected
 
-          response = app.call(request.env)
+      def dif_json(metadata_dataset)
+        dif_atom = Metadata::DifAtom.new
+        dif_json = dif_atom.dif(metadata_dataset)
+      end
 
-          if response.status < 300
-
-            metadata_dataset = ::Yajl::Parser.parse(response.io)
-
-            # if id false? and many => pack xml in somethimg p metadata_dataset
-  
-            xml = case request.format
-              when "dif", "xml"
-                then dif_xml(dif_json(metadata_dataset))
-              when "atom"
-                then atom_entry(metadata_dataset).to_xml
-              when "iso"
-                then iso(dif_xml(dif_json(metadata_dataset)))
-            end
-
-            if "validate" == request.path_info.split("/").last
-              dif = ::Gcmd::Dif.new
-              dif.load_xml xml
-              report = dif.validate_xml
-              [200, JSON_HEADER_HASH, [report.to_json]]
-            else
-              [200, XML_HEADER_HASH, [xml]]
-            end
+      def dif_xml(dif_json)
+        builder = ::Gcmd::DifBuilder.new
+        builder.build_dif( dif_json )
+      end
 
 
+      def atom_entry(metadata_dataset)
+        atom = metadata_dataset
+        entry = ::Atom::Entry.new do |e|
+          e.id = atom["dif:Entry_ID"] || "urn:uuid:#{atom["id"]}"
 
-          else
-            response
+          e.title = atom["title"]
+          e.summary = atom["summary"]
+
+          e.authors << ::Atom::Person.new(:name => 'John Doe')
+
+          atom["contributors"].each do |c|
+            e.contributors << ::Atom::Person.new(:name => c["first_name"]+" "+c["last_name"], :email => c["email"], :uri => c["uri"])
           end
-        end
 
-        protected
+          atom["links"].each do |link|
+            e.links << ::Atom::Link.new(:href => link["href"], :title => link["title"], :rel => link["rel"])
+          end
+          #e.links << ::Atom::Link.new(:href => ".atom", :type => "application/atom+xml", :rel => "self")
+          #e.links << ::Atom::Link.new(:href => ".json", :type => "application/json", :rel => "alternate")
+          #e.links << ::Atom::Link.new(:href => ".dif", :type => "application/dif+xml", :rel => "alternate")
 
-        def dif_json(metadata_dataset)
-          dif_atom = Metadata::DifAtom.new
-          dif_json = dif_atom.dif(metadata_dataset)
-        end
-  
-        def dif_xml(dif_json)
-          builder = ::Gcmd::DifBuilder.new
-          builder.build_dif( dif_json )
-        end
+          atom["categories"].each do |category|
+            e.categories << ::Atom::Category.new(:term => category["term"], :scheme => category["scheme"], :label => category["label"])
+          end
 
-
-        def atom_entry(metadata_dataset)
-          atom = metadata_dataset
-          entry = ::Atom::Entry.new do |e|
-            e.id = atom["dif:Entry_ID"] || "urn:uuid:#{atom["id"]}"
-  
-            e.title = atom["title"]
-            e.summary = atom["summary"]
-  
-            e.authors << ::Atom::Person.new(:name => 'John Doe')
-  
-            atom["contributors"].each do |c|
-              e.contributors << ::Atom::Person.new(:name => c["first_name"]+" "+c["last_name"], :email => c["email"], :uri => c["uri"])
-            end
-  
-            atom["links"].each do |link|
-              e.links << ::Atom::Link.new(:href => link["href"], :title => link["title"], :rel => link["rel"])
-            end
-            #e.links << ::Atom::Link.new(:href => ".atom", :type => "application/atom+xml", :rel => "self")
-            #e.links << ::Atom::Link.new(:href => ".json", :type => "application/json", :rel => "alternate")
-            #e.links << ::Atom::Link.new(:href => ".dif", :type => "application/dif+xml", :rel => "alternate")
-  
-            atom["categories"].each do |category|
-              e.categories << ::Atom::Category.new(:term => category["term"], :scheme => category["scheme"], :label => category["label"])
-            end
-  
-            if atom["source"] and atom["source"]["dif"] and atom["source"]["dif"]["Parameters"]
-              atom["source"]["dif"]["Parameters"].each do |p|
-                p.each do |k,v|
-                  e.categories << ::Atom::Category.new(:term => v, :scheme => "http://gcmd.gsfc.nasa.gov/Aboutus/xml/dif/##{k}")
-                end
-  
+          if atom["source"] and atom["source"]["dif"] and atom["source"]["dif"]["Parameters"]
+            atom["source"]["dif"]["Parameters"].each do |p|
+              p.each do |k,v|
+                e.categories << ::Atom::Category.new(:term => v, :scheme => "http://gcmd.gsfc.nasa.gov/Aboutus/xml/dif/##{k}")
               end
+
             end
-  
-            e.published = Time.parse(atom["published"])
-            e.updated = Time.parse(atom["updated"])
-  
-           # e.rights = atom["rights"]
-  
           end
-          entry
+
+          e.published = Time.parse(atom["published"])
+          e.updated = Time.parse(atom["updated"])
+
+         # e.rights = atom["rights"]
+
         end
+        entry
+      end
 
       def iso(dif)
         tmp = Tempfile.new('dif')
