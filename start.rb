c5a81370ed11fb9c233837cd075310ca0bf7679c
RDux <ruben.dens@gmail.com>
2012-08-10 13:54:38 +0200

    included lib/npolar/rack/middleware declaration

--- start.rb
+++ start.rb
@@ -1,18 +1,17 @@
 require "bundler/setup"
-
 require "yajl/json_gem" # https://github.com/brianmario/yajl-ruby
+
 # Server
-
-
 require "./lib/npolar/exception"
 require "./lib/npolar/rack/request"
 require "./lib/npolar/rack/response"
 require "./lib/npolar/rack/middleware"
 require "./lib/npolar/api"
 require "./lib/npolar/api/core"
+
+# Auth
 #require "./lib/npolar/auth"
 require "./lib/npolar/auth/couch"
-
 require "./lib/npolar/rack/authorizer"
 require "./lib/npolar/auth/ldap"
 
@@ -27,6 +26,7 @@ require "./lib/npolar/rack/require_param"
 require "./lib/metadata/rack/dif"
 require "./lib/npolar/rack/validate_id"
 require "./lib/npolar/rack/require_param"
+require "./lib/npolar/rack/middleware"
 
 require "rack/throttle"
 require "rack/contrib/jsonp"
