bef60d2824b1796922506f234bd425be97a4ec9c
RDux <ruben.dens@gmail.com>
2012-08-30 15:14:35 +0200

    added test for json schema validation

--- spec/metadata/dataset_spec.rb
+++ spec/metadata/dataset_spec.rb
@@ -9,9 +9,29 @@ describe Metadata::Dataset do
   
   context "object" do
     
-    it "should have an id if provided with one" do
-      dataset = Metadata::Dataset.new( {"id" => "abc"} )
-      dataset.id.should eq( "abc" )
+    it "should be a Hash" do
+      subject.should be_a_kind_of( Hash )
+    end
+    
+  end
+  
+  context "schema" do
+    
+    before( :each ) do
+      subject.schema = JSON.parse( File.read( "spec/data/NpMetaSchema.json" ) )
+    end
+    
+    it "should be invalid if the data doesn't meet schema requirements" do
+      subject.valid?.should == false
+    end
+    
+    it "should be valid if the minimum requirements set by the schema are met" do
+      subject.id = "abc"
+      subject.title = "test data"
+      subject.license = "http://creativecommons.org/licenses/by/3.0/no/"
+      subject.group = "biodiversity"
+      subject.progress = "completed"
+      subject.valid?.should == true
     end
     
   end
