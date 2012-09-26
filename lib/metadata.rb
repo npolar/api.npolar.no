7f9d435ef7030fb73a11a82741355ff6df6da98e
cnrdh <conrad.helgeland@gmail.com>
2012-09-26 11:34:49 +0200

    Updated

---
+++ lib/metadata.rb
@@ -0,0 +1,11 @@
+module Metadata
+
+  class << self
+    attr_accessor :collections
+  end
+
+ def self.workspace
+    "metadata"
+ end
+
+end
--- lib/metadata/dataset.rb
+++ lib/metadata/dataset.rb
@@ -19,6 +19,46 @@ module Metadata
   class Dataset < Hashie::Mash
     
     attr_accessor :schema
+
+    def self.country_codes
+    end
+
+    def self.sets
+      ["ARCTIC", "ANTARCTICA", "IPY:NO", "IPY", "Cryoclim", "NMDC"]
+    end
+
+    def self.oai_sets
+      [ {:spec => "ARCTIC", :name => "Arctic"},
+        {:spec => "ANTARCTICA", :name => "Antarctic"},
+        {:spec => "IPY:NO", :name => "IPY (Norway)", :description => "Norwegian contributions to the International Polar Year"},
+        {:spec => "IPY", :name => "IPY", :description => "Datasets from the International Polar Year (2007-2008)"},
+        {:spec => "Cryoclim", :name => "Cryoclim", :description => "Cold datasets"},
+        {:spec => "NMDC", :name => "Norwegian Marine Data Centre", :description => "Marine datasets"}
+      ]
+    end
+
+    def self.summary
+      "Discovery-level metadata, in particular DIFs targeted at NASA's Global Change Master Directory."
+    end
+#<IDN_Node>
+#<Short_Name>IPY</Short_Name>
+#</IDN_Node>
+#<IDN_Node>
+#<Short_Name>AMD/NO</Short_Name>
+#</IDN_Node>
+#<IDN_Node>
+#<Short_Name>ARCTIC/NO</Short_Name>
+#</IDN_Node>
+#<IDN_Node>
+#<Short_Name>ARCTIC</Short_Name>
+#</IDN_Node>
+#<IDN_Node>
+#<Short_Name>AMD</Short_Name>
+#</IDN_Node>
+#<IDN_Node>
+#<Short_Name>DOKIPY</Short_Name>
+#</IDN_Node>
+
     
     def to_solr
     end
