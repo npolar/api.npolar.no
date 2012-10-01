71bdd612db1f1d22b900ce979d9f98d4cdda2501
cnrdh <conrad.helgeland@gmail.com>
2012-10-01 15:32:43 +0200

    Moved pid to out of project folder

--- example/install/unicorn.rb
+++ example/install/unicorn.rb
@@ -6,8 +6,9 @@ timeout 30
 
 working_directory '/home/api/api.npolar.no' # git clone git@github.com:npolar/api.npolar.no.git
 listen "0.0.0.0:9000", :backlog => 1024
-#listen "/tmp/api-npolar.sock", :backlog => 1024
+# listen "api.npolar:80", :backlog => 1024
+# listen "/tmp/api-npolar.sock", :backlog => 1024
 
-pid '/home/api/api.npolar.no/api.pid'
+pid '/home/api/api.pid'
 stderr_path '/home/api/log/error.log'
 stdout_path '/home/api/log/out.log'
