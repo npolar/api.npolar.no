d35288eeed09769b1de56920a3b1b2e0c6d3fe7e
cnrdh <conrad.helgeland@gmail.com>
2013-01-16 13:51:20 +0100

    Added ssha (salted SHA1 password hash)

--- lib/npolar/auth/ldap.rb
+++ lib/npolar/auth/ldap.rb
@@ -1,10 +1,21 @@
 require "net/ldap"
+require "digest/sha1"
+require "base64"
 
 module Npolar
   module Auth
     class Ldap < Net::LDAP
 
       DEFAULT_DOMAIN = "npolar.no"
+
+      def self.ssha(password, salt)
+        "{SSHA}"+Base64.encode64(Digest::SHA1.digest(password+salt)+salt).chomp!
+      end
+    
+      def self.salt
+        Base64.encode64(Digest::SHA1.digest("#{rand(64)}/#{Time.now.to_f}/#{Process.pid}"))[0..7]
+      end
+
 
       def self.authenticator(domain=DEFAULT_DOMAIN)
         lambda { | ldap, request |
