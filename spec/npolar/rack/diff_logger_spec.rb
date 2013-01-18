6c0b55f63da22a614968daf26dd29d72ff81ee09
trevor <trevor@kauai.(none)>
2013-01-18 10:53:51 +0100

    more tests for diff_logger

--- spec/npolar/rack/diff_logger_spec.rb
+++ spec/npolar/rack/diff_logger_spec.rb
@@ -3,11 +3,16 @@ require "npolar/rack/diff_logger"
 
 describe Npolar::Rack::DiffLogger do
   before(:each) do
-    @env = Rack::MockRequest.env_for("/", "REQUEST_METHOD" => "PUT")
+    @env = Rack::MockRequest.env_for("/", 
+      "REQUEST_METHOD" => "PUT",
+      "REQUEST_URI" => "/abc/def/foo.json",
+      "rack.input" => StringIO.new('{ "_id":"org-polarresearch-303","_rev":"4-a0f42d79ee71b299b0917a634ddb7732","a" : "2", "b" : "3"}')
+    )
   end 
 
   subject do
-    app = [200, {"Content-type" => "application/json"}, [""]]
+    app = mock( "foobar", :call => Npolar::Rack::Response.new(
+             StringIO.new(""), 200, {"Content-Type" => "application/json"} ) )  
     data_storage = Npolar::Storage::Couch.new("diffs")
     diff_storage = Npolar::Storage::Couch.new("metadata_dataset")
 
@@ -41,6 +46,17 @@ describe Npolar::Rack::DiffLogger do
   end
   
   context "#handle" do
+    it "PUT should not result in 404" do
+      status, headers, body = subject.handle(Npolar::Rack::Request.new(@env))
+      status.should_not == 404
+    end
+
+    it "DELETE should not result in 404" do
+      @env["REQUEST_METHOD"] = "DELETE"
+      status, headers, body = subject.handle(Npolar::Rack::Request.new(@env))
+      status.should_not == 404
+    end
+
   end
 
 end
