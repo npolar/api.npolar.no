1b64cd24a2c68c3a303d6fe9295459dda500b58b
trevor <trevor@kauai.(none)>
2013-01-22 15:19:37 +0100

    Patched to allow in queries of the form west:[0 TO 2] .  These queries were getting screwed up by this:  qstar = "title:#{qstar} OR #{qstar} OR #{qstar}*"

--- lib/npolar/api/solr_query.rb
+++ lib/npolar/api/solr_query.rb
@@ -5,15 +5,17 @@ class Npolar::Api::SolrQuery
 
   def self.q(request)
     qstar = request["q"] ||= "*"
-    
-    if qstar =~ /^[\*]$|^\*\:\*$|^(\s+)?$/
-      qstar = "*:*"
-    else
-      #unless qstar =~ /\*/
-      #  qstar = qstar.downcase #+"*"
-      #end
-      qstar = "title:#{qstar} OR #{qstar} OR #{qstar}*"
+   
+    if not qstar =~ /^[^\*]+:.+$/ 
+      if qstar =~ /^[\*]$|^\*\:\*$|^(\s+)?$/
+        qstar = "*:*"
+      else
+        #unless qstar =~ /\*/
+        #  qstar = qstar.downcase #+"*"
+        #end
+        qstar = "title:#{qstar} OR #{qstar} OR #{qstar}*"
       end
+    end
     qstar
   end
 end
