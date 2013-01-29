55dc4614ea9573c88c47de0e6bf57b1defe64fe2
RDux <ruben.dens@gmail.com>
2013-01-29 16:27:24 +0100

    A middleware that removes all empty strings, hashes, arrays and nil values from a json input

---
+++ lib/npolar/rack/json_cleaner.rb
@@ -0,0 +1,74 @@
+module Npolar
+  module Rack
+    
+    # [Functionality]
+    #   * Clean empty values from json documents.
+    #
+    # [License]
+    #   This code is licensed under the {http://www.gnu.org/licenses/gpl.html GNU General Public License Version 3} (GPLv3)
+    #
+    # @author Ruben Dens
+    
+    class JsonCleaner < Npolar::Rack::Middleware
+      
+      def condition?(request)
+        create?(request)
+      end
+      
+      def handle(request)
+        log.info "@JsonCleaner: Cleaning input!!!"
+        t0 = Time.now
+        data = Yajl::Parser.parse(request.body.read)
+        
+        cleaned = clean(data)
+        request.env["rack.input"] = StringIO.new(cleaned.to_json)
+        
+        log.info "@JsonCleaner: Input cleaned in #{Time.now - t0}"
+        app.call(request.env)     
+      end
+      
+      def create?(request)
+        ["PUT", "POST"].include?(request.request_method)
+      end
+      
+      def clean(data)
+        if data.is_a? Array
+          return clean_array(data)
+        elsif data.is_a? Hash
+          return clean_hash(data)
+        end
+        
+        data
+      end
+      
+      def clean_hash( data )
+        data.reject!{|k,v| !v.is_a?( Float ) && (v.nil? || v.empty? )}
+        
+        data.each do |k,v|
+          if v.is_a? Hash
+            data[k] = clean_hash(v)
+          elsif v.is_a? Array
+            data[k] = clean_array(v)
+          end
+        end
+        
+        data
+      end
+      
+      def clean_array( data )
+        data.reject!{|e| !e.is_a?( Float ) && (e.nil? || e.empty?)}
+        
+        data.each_with_index do |e, i|
+          if e.is_a? Hash
+            data[i] = clean_hash(e)
+          elsif e.is_a? Array
+            data[i] = clean_array(e)
+          end
+        end
+        
+        data
+      end
+      
+    end
+  end
+end
