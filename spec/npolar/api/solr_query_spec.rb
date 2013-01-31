e8c5d73d437ec4d721cb7b143340ebf11f8b1abb
cnrdh <conrad.helgeland@gmail.com>
2013-01-31 12:59:26 +0100

    Started work on from..to range queries

--- spec/npolar/api/solr_query_spec.rb
+++ spec/npolar/api/solr_query_spec.rb
@@ -1,7 +1,14 @@
 require "spec_helper"
-require "npolar/api/solr_query.rb"
+require "npolar/api"
+require "npolar/rack/request"
+require "npolar/api/solr_query"
 
 describe Npolar::Api::SolrQuery do
+  def rack_request(path="/", *args)
+    env = Rack::MockRequest.env_for(path, *args)
+    Npolar::Rack::Request.new(env)
+  end
+
   context "#self.q" do
     it "blank query should yield *:*" do
       Npolar::Api::SolrQuery::q({"q" => ""}).should == "*:*"
@@ -17,6 +24,25 @@ describe Npolar::Api::SolrQuery do
 
     it "foo should yield title:foo OR foo OR foo*" do
       Npolar::Api::SolrQuery::q({"q" => "foo"}).should == "title:foo OR foo OR foo*"
+    end
+  end
+
+  context "#request" do
+    it "should return a Rack::Request"
+    it "should raise Exception if Rack::Request is not set"
+  end
+
+
+  context "range queries (from..to)" do
+    before (:each) do
+      @solr_query = Npolar::Api::SolrQuery.new
+      @solr_query.request = rack_request("/foo?bar=0..9&north=-90.0..-60.0,60.0..90.0&not-range=123")
+    end
+    describe "#ranges" do
+
+      it "#ranges should return all parameters containing .." do
+        @solr_query.ranges.should == []
+      end
     end
   end
 end
