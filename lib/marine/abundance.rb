ba29f5ac36f3555c79be9636001ef16ff0a2fea4
trevor <trevor@npolar.no>
2013-02-22 11:27:09 +0100

    'animals' is now species info as a hash in couch, adjust mapping to flat fields in solr doc accordingly

--- lib/marine/abundance.rb
+++ lib/marine/abundance.rb
@@ -11,19 +11,29 @@ module Marine
     end
 
     def to_solr
-      doc = self
+      doc = self.to_hash
       id = doc["id"] ||= doc["_id"]
       rev = doc["rev"] ||= doc["_rev"] ||= nil
 
-      solr = doc
+      solr = {}
       solr["id"] = id
       solr["rev"] = rev
+
+      doc.each do |k, v|
+        if k == "animal"
+          doc["animal"].each do |ani_k, ani_v|
+            solr[ani_k] = ani_v
+          end
+        elsif !["id", "rev", "_id", "_rev"].include? k 
+          solr[k] = v
+        end
+      end 
  
       text = ""
-      self.to_hash.each do |k,v|
+      solr.each do |k,v|
         text += "#{k} = #{v} | "
       end
-      solr[:text] = text
+      solr["text"] = text
 
       pp solr
       solr
