0253846e20617cc7f07bd0ce69fed377fee80d04
trevor <trevor@npolar.no>
2013-03-11 14:03:19 +0100

    Break sampledepth into two fields "from" and "to"

--- lib/marine/samples.rb
+++ lib/marine/samples.rb
@@ -6,7 +6,7 @@ module Marine
 
     def self.facets
       [
-        "animal_group", "conveyance", "gear", "institution", "preservation", "programs", "sample_types", "station", "substation", "status"
+        "workspace", "collection", "animal_group", "conveyance", "gear", "institution", "preservation", "programs", "sample_types", "station", "substation", "status"
       ]
     end
 
@@ -16,32 +16,36 @@ module Marine
       rev = doc["rev"] ||= doc["_rev"] ||= nil
 
       solr = {
-        :id             => id,
-        :rev            => rev,
-        :animal_group   => doc.fetch("animal_group", ""),
-        :bottomdepth    => doc.fetch("bottomdepth", ""),
-        :conveyance     => doc.fetch("conveyance", ""),
-        :ctdnr          => doc.fetch("ctdnr", ""),
-        :filteredwater  => doc.fetch("filteredwater", ""),
-        :flowmeter      => doc.fetch("flowmeter", ""),
-        :gear           => doc.fetch("gear", ""),
-        :institution    => doc.fetch("institution", ""),
-        :instref        => doc.fetch("instref", ""),
-        :local_date     => doc.fetch("local_date", ""),
-        :metadata_id    => doc.fetch("metadata_id", ""),
-        :name           => doc.fetch("name", ""),
-        :position_end   => doc.fetch("position_end", ""),
-        :position_start => doc.fetch("position_start", ""),
-        :preservation   => doc.fetch("preservation", ""),
-        :processed_date => doc.fetch("processed_date", ""),
-        :programs       => doc.fetch("programs", ""),
-        :sample_name    => doc.fetch("sample_name", ""),
-        :sample_types   => doc.fetch("sample_types", ""),
-        :sampledepth    => doc.fetch("sampledepth", ""),
-        :station        => doc.fetch("station", ""),
-        :status         => doc.fetch("status", ""),
-        :substation     => doc.fetch("substation", ""),
-        :utc_date       => doc.fetch("utc_date", "")
+        :id                => id,
+        :rev               => rev,
+        :animal_group      => doc.fetch("animal_group", ""),
+        :bottomdepth       => doc.fetch("bottomdepth", ""),
+        :conveyance        => doc.fetch("conveyance", ""),
+        :ctdnr             => doc.fetch("ctdnr", ""),
+        :filteredwater     => doc.fetch("filteredwater", ""),
+        :flowmeter         => doc.fetch("flowmeter", ""),
+        :gear              => doc.fetch("gear", ""),
+        :institution       => doc.fetch("institution", ""),
+        :instref           => doc.fetch("instref", ""),
+        :local_date        => doc.fetch("local_date", ""),
+        :metadata_id       => doc.fetch("metadata_id", ""),
+        :name              => doc.fetch("name", ""),
+        :position_end      => doc.fetch("position_end", ""),
+        :position_start    => doc.fetch("position_start", ""),
+        :preservation      => doc.fetch("preservation", ""),
+        :processed_date    => doc.fetch("processed_date", ""),
+        :programs          => doc.fetch("programs", ""),
+        :sample_name       => doc.fetch("sample_name", ""),
+        :sample_types      => doc.fetch("sample_types", ""),
+        :sample_depth_from => doc.fetch("sample_depth_from", ""),
+        :sample_depth_to   => doc.fetch("sample_depth_to", ""),
+        :station           => doc.fetch("station", ""),
+        :status            => doc.fetch("status", ""),
+        :substation        => doc.fetch("substation", ""),
+        :title             => doc.fetch("title", ""),
+        :utc_date          => doc.fetch("utc_date", ""),
+        :workspace         => "marine",
+        :collection        => "samples"
       }
 
       if doc.has_key?("staff")
