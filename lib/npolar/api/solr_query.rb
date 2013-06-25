2a516d3b32f1a27fccde4da2c023a13d7e359f74
cnrdh <conrad.helgeland@gmail.com>
2013-06-25 09:42:56 +0200

    Rolled back and simplified

--- lib/npolar/api/solr_query.rb
+++ lib/npolar/api/solr_query.rb
@@ -1,58 +1,19 @@
-# JSON feed writer for the "api" Solr core
+# 
+
+# JSON feed writer for the "api" Solr core, see
 class Npolar::Api::SolrQuery
 
-  attr_writer :request
-
-  def request
-    if @request.nil?
-      raise ArgumentError "Rack::Request instance missing"
-    end
-    @request
-  end
-
   def self.q(request)
-    query = request["q"]
-    if query.nil? || query.empty?
-      return "*"
-    end
-   
-    ranges = self.ranges(request["q"])
-       
-    if !ranges.empty? 
-      ranges = ranges.map { |range| self.fq_range(range[0], range[1], range[2]) }
-      query = ranges.join(" AND ")
-      puts query
-
-    elsif query =~ /^[^\*]+:.+$/ 
-      # remove any whitespace around :
-      query = query.gsub(/\s*:\s*/, ':')
-
-      # ensure 'TO' doesn't appear in any other case
-      query = query.gsub(/to/i, 'TO')
-
-    elsif query =~ /^[\*]$|^\*\:\*$|^(\s+)?$/
-        query = "*:*"
-
+    qstar = request["q"] ||= "*"
+    
+    if qstar =~ /^[\*]$|^\*\:\*$|^(\s+)?$/
+      qstar = "*:*"
     else
-        #unless query =~ /\*/
-        #  query = query.downcase #+"*"
-        #end
-      query = "title:#{query} OR #{query} OR #{query}*"
-    end
-
-    return query
+      #unless qstar =~ /\*/
+      #  qstar = qstar.downcase #+"*"
+      #end
+      qstar = "title:#{qstar} OR #{qstar} OR #{qstar}*"
+      end
+    qstar
   end
-
-  def self.ranges(query)
-    query.scan(/\s*(\w+)=([0-9\-]*)?\.\.([0-9\-]*)?\s*/)
-  end
-
-  # @param range start..end
-  # @return string "field:[start TO end]"
-  def self.fq_range(field, from, to)
-    from ||= "*"
-    to ||= "*"
-    "#{field}:[#{from} TO #{to}]"
-  end
-
 end
