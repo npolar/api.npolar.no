bd499e73b43c987541b63964b4426ce12b1187a8
cnrdh <conrad.helgeland@gmail.com>
2013-08-13 18:12:03 +0200

    Create Elasticsearch index on 201

--- lib/service.rb
+++ lib/service.rb
@@ -1,4 +1,5 @@
 require "hashie"
+require "faraday"
 # Service (Service description document)
 # Handles service descriptions at /service and service configuration
 # Schema: http://api.npolar.no/schema/api
@@ -8,12 +9,33 @@ class Service < Hashie::Mash
 
   def self.after_lambda
     lambda {|request, response|
-      if ["POST","PUT"].include? request.request_method
+      
+      if response.is_a? Array and response.size == 3
+        status = response[0]
+      else
+        response.status
+      end
+
+      if ["POST","PUT"].include? request.request_method and 201 == status
         
         service = self.new(JSON.parse(request.body.read))
+
         bootstrap = Npolar::Api::Bootstrap.new
         bootstrap.log = Npolar::Api.log
         bootstrap.create_database(service)
+
+        if service.search? and service.search.engine =~ /Elasticsearch/i
+          elastic = service.search
+          
+          status = Faraday.get("#{elastic.uri}/#{elastic["index"]}/_status").status
+          if 404 == status # head does not work for status...            
+            index = Faraday.put("#{elastic.uri}/#{elastic["index"]}")
+            log = Npolar::Api.log
+            log.info "Created Elasticsearch index #{elastic["index"]} #{index.status}: #{index.body}"
+          end
+          #elastic = Npolar::ElasticSearch::Client.new(request, {:uri => service.search.uri, :index => service.search["index"] })
+          #elastic.create_index
+        end
 
       end
       response
