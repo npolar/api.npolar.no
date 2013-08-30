0771d3d0f4b1fe957b4b8d675f30eee7387a8aea
RDux <ruben.dens@gmail.com>
2013-08-30 11:48:03 +0200

    using or filter. downcase values. replace space by comma

--- lib/npolar/elasticsearch/query.rb
+++ lib/npolar/elasticsearch/query.rb
@@ -2,7 +2,7 @@ require 'logger'
 
 module Npolar
   module ElasticSearch
-  
+
     # [Functionality]
     #   This class is used to constructs a json query
     #   for Elasticsearch
@@ -42,9 +42,9 @@ module Npolar
     #
     # [Authors]
     #   - Ruben Dens
-    
+
     class Query
-      
+
       def initialize(configuration = {})
         self.params = params
         if configuration.is_a?( Hash )
@@ -54,20 +54,20 @@ module Npolar
             Expected a Hash but got |#{configuration.class}|. Falling back to defaults."
         end
       end
-      
+
       def config=configuration
         @cfg = configuration
       end
-      
+
       def config
         @cfg ||= {}
       end
-      
+
       # List of valid configuration items
       def configurable
         [:start, :limit, :filters, :facets, :date_facets, :sort]
       end
-      
+
       # Build an elasticsearch query body from the provided parameters and configuration
       def build
         if params.select{|k,v| k.match(/^filter-(.*)/)}.empty? and !config.has_key?(:filters)
@@ -75,34 +75,34 @@ module Npolar
         else
           body = { :query => filtered_query }
         end
-        
+
         body[:from] = from
         body[:size] = size
         body[:sort] = sort
         body[:facets] = facets unless facets.empty?
         body[:fields] = fields unless fields.nil?
-        
+
         log.debug "Npolar::ElasticSearch::Query:\n#{JSON.pretty_generate(body)}"
-        
+
         body.to_json
       end
-      
+
       def from
         params['start'] ? params['start'].to_i : config[:start] ||= 0
       end
-      
+
       def size
         params['limit'] ? params['limit'].to_i : config[:limit] ||= 25
       end
-      
+
       # Date facets are only supported through configuration
       # to define a date facet you provide this in a config hash
       # {:date_facets => { "year" => {"created" => "year"}, "published" => {} }
       # @see {http://www.elasticsearch.org/guide/reference/api/search/facets/date-histogram-facet/ Date histogram}
-      
+
       def date_facets
         df = {}
-        
+
         if config[:date_facets]
           config[:date_facets].each do |facet|
             df["#{facet[:interval]}-#{facet[:field]}"] = {
@@ -113,19 +113,19 @@ module Npolar
             }
           end
         end
-        
+
         df
       end
-      
+
       def facet_params
         fp = params.select{|k, v| k == 'facets' }.map{|k,v| v.split(',')}.flatten
         fp.concat(config[:facets]) if config[:facets]
         fp
       end
-      
+
       def facets
         fc = {}
-        
+
         facet_params.map{ |field|
           {
             field => {
@@ -137,14 +137,14 @@ module Npolar
         }.each{ |facet|
           fc.merge!(facet)
         }
-        
+
         fc.merge!(date_facets)
       end
-      
+
       def fields
         params['fields'] ? params['fields'].split(',') : nil
       end
-      
+
       def filtered_query
         {
           :filtered => {
@@ -153,36 +153,40 @@ module Npolar
           }
         }
       end
-      
+
       # Gather all parameters and configuration items that define a filter
       def filter_params
         fp = params.select{|k, v| k =~ /^filter-(.*)/} # Grab filters from the query parameters
         config[:filters].each{|filter| fp.merge!(filter)} if config.has_key?(:filters) # Merge in configured filters
         fp
       end
-      
+
       # Build a filter from the filter parameters
       # @see #filter_params
       def filter
         {
-          :and => filter_params.map{ |k,v|
+          :or => filter_params.map{ |k,v|
+            # Remove any spaces from the filters
+            v.gsub!(/\s+/, ',')
+
+            # Split and map to proper query
             v.split(',').map{|value|
               unless value.match(/\-?\d+Z?\.\.\-?\d+/)
                 {
                   :term => {
-                    k.to_s.gsub(/^filter-/, '') => value
+                    k.to_s.gsub(/^filter-/, '') => value.downcase
                   }
                 }
               else
                 vals = value.split('..')
-                
+
                 # Swap the values if the second one is bigger then the first
                 unless value.match(/^\d{4}\-(\d{2})?\-?(\d{2})?T?(\d{2}):?(\d{2})?:?(\d{2})?Z?/)
                   unless vals[0].to_f < vals[1].to_f
                     vals[0], vals[1] = vals[1], vals[0]
                   end
                 end
-                
+
                 {
                   :range => {
                     k.to_s.gsub(/^filter-/, '') => {
@@ -192,12 +196,12 @@ module Npolar
                   }
                 }
               end
-              
+
             }
           }.flatten
         }
       end
-      
+
       # Returns the correct query type
       # @see #field_query
       # @see #query_string
@@ -205,7 +209,7 @@ module Npolar
         return field_query unless params.select{|k,v| k.match(/^q-(.*)/)}.empty?
         query_string
       end
-      
+
       def query_string
         {
           :query_string => {
@@ -214,7 +218,7 @@ module Npolar
           }
         }
       end
-      
+
       # Query parameter q. If no q parameter is present it uses a wildcard.
       # If a q paramter is present it appends a wildcard to support fuzzy searches
       def q_param
@@ -224,19 +228,19 @@ module Npolar
           params['q'] = '*'
         end
       end
-      
+
       def sort
         sort_items = []
-        
+
         if params['sort']
           sort_items = params['sort'].split(',').map{|item|
             item.match(/^\-(.*)/) ? {item[1..-1] => :desc} : {item => :asc}
           }
         end
-        
+
         sort_items
       end
-      
+
       # Build a query to match a specific data field
       def field_query
         {
@@ -250,21 +254,21 @@ module Npolar
           }.first
         }
       end
-      
+
       def params=(parameters)
         @params = parameters
       end
-      
+
       def params
         @params ||= {'q' => '*'}
       end
-      
+
       protected
-      
+
       def log
         Logger.new(STDERR)
       end
-      
+
     end
   end
 end
