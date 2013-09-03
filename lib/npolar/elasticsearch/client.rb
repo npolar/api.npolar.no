4f02fa78a7634870d84dac441c5c597b4fe8feab
RDux <ruben.dens@gmail.com>
2013-09-03 11:04:42 +0200

    Added exception handling

--- lib/npolar/elasticsearch/client.rb
+++ lib/npolar/elasticsearch/client.rb
@@ -1,7 +1,7 @@
 module Npolar
   module ElasticSearch
     class Client
-      
+
       CONFIG = {
         :uri => "http://localhost:9200/",
         :index => "",
@@ -9,36 +9,47 @@ module Npolar
         :start => 0,
         :limit => 25
       }
-      
+
       attr_accessor :request, :config, :log
-      
+
       def initialize(request, config = {})
         self.request = request
         self.config = CONFIG.merge(config)
         self.log = ::Logger.new(STDERR)
       end
-      
+
       def search
         query = Npolar::ElasticSearch::Query.new(config)
         query.params = request.params
         query = query.build
-        
+
         response = http.post do |req|
           req.url "/#{config[:index]}/#{config[:type]}/_search"
           req.headers['Content-Type'] = 'application/json; charset=utf8'
           req.body = query
         end
-        
-        result = Npolar::ElasticSearch::Result.new(request, response)
-        if request.params['format'] && request.params['format'] == 'csv'
-          feed = result.to_csv
+
+        unless [500, 404].include?(response.status)
+          result = Npolar::ElasticSearch::Result.new(request, response)
+
+          if request.params['format'] && request.params['format'] == 'csv'
+            feed = result.to_csv
+          else
+            feed = result.to_feed
+          end
+
+          headers = {"Content-Type" => "application/json; charset=utf8","Content-Length" => feed.bytesize}
+          Rack::Response.new(feed, 200, headers)
         else
-          feed = result.to_feed
+
+          response = case response.status
+            when 404 then Rack::Response.new('Search engine not found', 404, {"Content-Type" => "text/plain; charset=utf8"})
+            when 500 then Rack::Response.new('Search engine error', 500, {"Content-Type" => "text/plain; charset=utf8"})
+          end
+
         end
-        headers = {"Content-Type" => "application/json; charset=utf8","Content-Length" => feed.bytesize}
-        Rack::Response.new(feed, 200, headers)
       end
-      
+
       def index( data )
         if data.is_a?( Array )
           log.info "Npolar::Elasticsearch::Client - Attempting to save #{data.size} documents to the #{config[:index]} index"
@@ -53,7 +64,7 @@ module Npolar
       end
 
       protected
-      
+
       def http
         Faraday.new(:url => config[:uri]) do |faraday|
           faraday.request  :url_encoded             # form-encode POST params
@@ -61,7 +72,7 @@ module Npolar
           faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
         end
       end
-      
+
     end
   end
 end
