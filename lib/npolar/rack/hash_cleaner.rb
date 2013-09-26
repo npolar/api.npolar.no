c9c4c51d60cd638479f9990921258eb4f9b85110
RDux <ruben.dens@gmail.com>
2013-09-26 07:26:39 +0200

    Renamed to HashCleaner to better match purpose. Improved to support all suported json data types

--- lib/npolar/rack/hash_cleaner.rb
+++ lib/npolar/rack/hash_cleaner.rb
@@ -2,14 +2,14 @@ module Npolar
   module Rack
 
     # [Functionality]
-    #   * Clean empty values from json documents.
+    #   * Clean empty values from hashes.
     #
     # [License]
     #   This code is licensed under the {http://www.gnu.org/licenses/gpl.html GNU General Public License Version 3} (GPLv3)
     #
     # @author Ruben Dens
 
-    class JsonCleaner < Npolar::Rack::Middleware
+    class HashCleaner < Npolar::Rack::Middleware
 
       def condition?(request)
         create?(request)
@@ -31,6 +31,7 @@ module Npolar
         ["PUT", "POST"].include?(request.request_method)
       end
 
+      # Double clean call to remove
       def clean(data)
         cleaned = clean_content(data)
         clean_content(cleaned)
@@ -50,7 +51,7 @@ module Npolar
         # When not a Float or Boolean.
         # And the value is nil or empty.
         # Remove the key-value pair.
-        data.reject!{|k,v| (!v.is_a?( Float ) && ![true,false].include?(v)) && (v.nil? || v.empty? )}
+        data.reject!{|k,v| (!number?(v) && !boolean?(v)) && (v.nil? || v.empty? )}
 
         # Loop through the remaining items and clean
         data.each do |k,v|
@@ -64,10 +65,20 @@ module Npolar
         # When not a Float or Boolean.
         # And the element is nil or empty.
         # Remove the element.
-        data.reject!{|e| (!e.is_a?( Float ) && ![true,false].include?(e)) && (e.nil? || e.empty?)}
+        data.reject!{|e| (!number?(e) && !boolean?(e)) && (e.nil? || e.empty?)}
 
         # Loop remaining elements and clean
         data.map!{|e| clean(e)}
+      end
+
+      # Check if item is Boolean
+      def boolean?( item )
+        [true,false].include?( item )
+      end
+
+      # Check if item is a kind of number
+      def number?( item )
+        [Float, Fixnum, Integer].any? {|type| item.is_a?( type )}
       end
 
     end
