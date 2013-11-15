8172f3961353cbc9745c3797121eecc5b117a7fa
cnrdh <conrad.helgeland@gmail.com>
2013-11-15 12:45:34 +0100

    Added static base method

--- lib/npolar/api.rb
+++ lib/npolar/api.rb
@@ -2,17 +2,17 @@ module Npolar
   module Api
 
     class << self
-      URI = "http://api.npolar.no/"
-      attr_writer   :log
+      attr_writer :log, :base
+    end
+
+    def self.base
+      @base||="http://api.npolar.no"
     end
     
     def self.facets
      ["collection", "workspace", "accept_mimetypes", "accept_schemas", "formats", "relations", "topics", "sets", "category", "country",
       "draft", "editors", "hemisphere", "investigators", "licences", "link", "methods", "parameter", "person", "placename", "project",
       "source", "iso_topics", "tags"]
-    end
-
-    def self.factory(name)
     end
 
     def self.log
