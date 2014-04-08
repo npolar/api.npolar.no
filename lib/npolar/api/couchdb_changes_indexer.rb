03765e09238e50ed460713802798314137601213
cnrdh <conrad.helgeland@gmail.com>
2014-04-08 10:30:51 +0200

    Call #to_solr (CouchDB changes indexer)

--- lib/npolar/api/couchdb_changes_indexer.rb
+++ lib/npolar/api/couchdb_changes_indexer.rb
@@ -6,7 +6,10 @@ require "em-http"
 require "yajl"
 require "elasticsearch"
 
+
+require_relative "../../../load"
 require "npolar/api/client"
+
 require_relative "../validation"
 require_relative "../../service"
 
@@ -52,7 +55,7 @@ module Npolar
           keepalive = 0
           http.stream do |chunk|
             begin 
-            keepalive = chunk.match(/^r?\n$/) ? keepalive + 1 : 0
+              keepalive = chunk.match(/^r?\n$/) ? keepalive + 1 : 0
             rescue
               keepalive = 0
             end
@@ -67,7 +70,11 @@ module Npolar
           end
         
           http.errback {log.error "#{http.error}" ;EM.stop}
-          http.callback {log.fatal "Stream finished"; EM.stop}
+          http.callback {
+            log.fatal "Stream finished"
+            EM.stop
+            # here the last seq should be persisted for restartof indexing at sa
+            }
         }
 
       end
@@ -87,7 +94,7 @@ module Npolar
                 uri.path = uri.path.gsub(/\/$/, "") + "/" + config.core
                 uri = uri.to_s
               end
-              
+
               log.debug "Solr #{uri} indexing #{changes.size} documents"
               
               updates = changes.select {|c|
@@ -98,6 +105,17 @@ module Npolar
               
               # Index
               docs = updates.select {|u| u[:deleted].nil? or u[:deleted] != true }.map {|d| d[:doc]}
+              
+              if service.model?
+                log.debug "Mapping to Solr using #{service.model}#to_solr"
+                docs.map! {|d|
+                  m = Npolar::Factory.constantize(service.model).new(d)
+                  m.to_solr
+                  
+                  }
+              end
+              
+              
               client = Npolar::Api::Client::JsonApiClient.new
               responses = client.post(docs, "#{uri}/update")
               
@@ -122,13 +140,46 @@ module Npolar
               idx = config[:index]
               type = config.type
               
-              changes.map! do |e|
-                operation = e[:deleted] ? :delete : :index
-                { operation => {:_index => idx, _type: type, _id: e[:id], data: e[:doc] } }
-              end
+              #changes.map! do |e|
+              #  operation = e[:deleted] ? :delete : :index
+              #  { operation => {:_index => idx, _type: type, _id: e[:id], data: e[:doc] } }
+              #end
+              #
+              #changes.map! do |e|
+              #  if e[:deleted]
+              #    {:delete => {:_index => search[:index], :_type => search.type, :_id => e[:id], :ignore => 404}}
+              #  else
+              #    {:index => {:_index => search[:index], :_type => search.type, :_id => e[:id], :data => e[:doc]}}
+              #  end
+              #end
+              updates = changes.select {|c|
+                c.key?(:doc) and
+                c.key?(:id) and
+                c[:id] !~ /_design\//
+              }
               
-              client = Elasticsearch::Client.new({url: url})
-              client.bulk :body => changes
+              # Index
+              docs = updates.select {|u| u[:deleted].nil? or u[:deleted] != true }.map {|d| d[:doc]}
+              uri = URI.parse(url)
+              uri.path = "/#{idx}/#{type}"
+              log.debug uri.to_s
+              client = Npolar::Api::Client::JsonApiClient.new(uri.to_s)
+              client.slice=1
+              docs.each do |d|
+                
+              responses = client.post(d.to_json)
+              end
+              #responses = client.post(docs, "#{url}")
+              
+              # @todo POST errors
+              # @todo DELETE, must match /get?id=id revision, see http://wiki.apache.org/solr/RealTimeGet
+              responses = []
+              
+              log.debug "Responses: #{responses.map {|r| r.code}}"
+              
+              #puts changes.to_json
+              #client = Elasticsearch::Client.new({url: url})
+              #client.bulk :body => changes
               
             }
           else
@@ -175,7 +226,7 @@ Options:\n"
           path = URI.parse(endpoint).path
           indexer.service = Service.services.select {|s| s.path == path }[0]
           
-          log.debug "#{CMD} #{endpoint}"
+          log.debug "#{CMD} #{endpoint} #{indexer.service.model}"
           indexer.run
           log.info "#{CMD} finished"
           
