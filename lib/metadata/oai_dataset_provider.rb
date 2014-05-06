5291a7d2375f0389f2e07e5320be80b089af121c
cnrdh <conrad.helgeland@gmail.com>
2014-05-06 11:53:37 +0200

    New /dataset/oai sets and improved OAI documentation

--- lib/metadata/oai_dataset_provider.rb
+++ lib/metadata/oai_dataset_provider.rb
@@ -1,18 +1,26 @@
 require "oai/provider"
 
-#module OAI::Provider::Response
-#  class RecordResponse
-#    # Monkey patch #timestamp_for because it expects the timestamp_field to r
-#    # /home/ch/github.com/ruby-oai/lib/oai/provider/response/record_response.rb
-#    def timestamp_for(record)
-#      record.send(provider.model.timestamp_field)
-#    end
-#  end
-#end
-
 module Metadata
   class OaiDatasetProvider < ::OAI::Provider::Base
 
+    def self.topic_sets
+      ["glaciology", "marine"]
+    end
+
+    def self.oai_sets
+      sets = [ {:spec => "arctic", :name => "Arctic datasets"},
+        {:spec => "antarctic", :name => "Antarctic datasets"},
+        {:spec => "ipy.org", :name => "International Polar Year", :description => "Datasets from the International Polar Year (2007-2008), see http://www.ipy.org"},
+        {:spec => "cryoclim.net", :name => "Cryoclim", :description => "Climate monitoring of the cryosphere, see http://cryoclim.net"},
+        {:spec => "gcmd.nasa.gov", :name => "Global Change Master Directory" }
+      ]+self.topic_sets.map {|t| { spec: t, name: t.capitalize+" datasets", description: "See http://api.npolar.no/dataset/?q=&filter-topics=#{t}" } }
+      sets
+    end
+    
+    def self.sets
+      topic_sets + oai_sets.map {|os| os[:spec]}
+    end
+    
     class << self
       client = Npolar::Api::Client::JsonApiClient.new(ENV["NPOLAR_API_COUCHDB"].gsub(/[\/]$/, "")+"/dataset")
       client.model = ::Metadata::Dataset.new
@@ -21,17 +29,38 @@ module Metadata
     
     # http://www.openarchives.org/OAI/2.0/oai-identifier.xsd#repositoryIdentifierType
     # /[a-zA-Z][a-zA-Z0-9\-]*(\.[a-zA-Z][a-zA-Z0-9\-]*)+/
-    # @todo declare "deleted records" support (how?)
 
     register_format ::Metadata::OaiDirectoryInterchangeFormat.instance
-    repository_name  "Norwegian Polar Institute's OAI-PMH service for dataset metadata"
+    repository_name  "Norwegian Polar Institute's OAI provider for dataset metadata"
     repository_url  "#{ENV["NPOLAR_API"].gsub(/^https/, "http")}/dataset/oai"
     record_prefix "oai:npolar.no:dataset"
     admin_email "data@npolar.no"
     source_model ::Metadata::OaiDumbCouchDbModel.new(@@client)
-    extra_description  "Norwegian Polar Institute's datasets (from 2008 to present).
-      See also http://api.npolar.no/dataset/?q= and http://data.npolar.no/datasets for more information"
-    sample_id "3925805c-3ab1-44bd-99bb-a0b26321953f"
+    extra_description  "OAI-PMH provider for Norwegian Polar Institute's datasets as DIF XML
     
+      See http://api.npolar.no/dataset/?q= for a searchable Dataset API that delivers 
+      metadata of datasets in JSON, CSV, DIF, or ISO 19000 formats.
+      
+      See also: http://data.npolar.no/datasets
+      
+      For DIF XML, see: http://gcmd.gsfc.nasa.gov/add/difguide/index.html
+    
+      http://api.npolar.no/dataset/oai supports all 6 verbs from v2 spec http://www.openarchives.org/OAI/openarchivesprotocol.html#ProtocolMessages
+        * http://api.npolar.no/dataset/oai?verb=Identify
+        * http://api.npolar.no/dataset/oai?verb=ListIdentifiers
+        * http://api.npolar.no/dataset/oai?verb=ListMetadataFormats
+        * http://api.npolar.no/dataset/oai?verb=ListSets (currently:#{self.sets.to_json})
+        * http://api.npolar.no/dataset/oai?verb=GetRecord&amp;metadataPrefix=dif&amp;identifier=0323b588-5023-57d1-bf98-201cd8192730
+        * http://api.npolar.no/dataset/oai?verb=ListRecords&amp;metadataPrefix=dif
+       
+      Time range
+        * http://api.npolar.no/dataset/oai?verb=ListIdentifiers&amp;from=2013-11-01&amp;until=2013-11-15&amp;metadataPrefix=dif
+      Sets (and time range)
+        * http://api.npolar.no/dataset/oai?verb=ListIdentifiers&amp;from=2013-11-01&amp;until=2099-01-01&amp;metadataPrefix=dif&amp;set=cryoclim.net
+    "
+    sample_id "3925805c-3ab1-44bd-99bb-a0b26321953f"
+    deletion_support OAI::Const::Delete::PERSISTENT
+    update_granularity OAI::Const::Granularity::HIGH
+ 
   end
 end
