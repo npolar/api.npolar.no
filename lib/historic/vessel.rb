1f444567530e424be7aea4737dc23367948cf8b0
cnrdh <cnrdh@users.noreply.github.com>
2014-10-20 20:20:59 +0200

    Historic vessel API

---
+++ lib/historic/vessel.rb
@@ -0,0 +1,104 @@
+require "hashie/mash"
+require "nori"
+require "json"
+module Historic
+class Vessel < Hashie::Mash
+    
+  #include Npolar::Validation::MultiJsonSchemaValidator
+  
+  def schemas
+    ["historic-vessel.json"] 
+  end
+  
+  def self.seed(filename_xml="#{__dir__}/../../seed/historic/vessel/Skip.xml")
+    nori = Nori.new
+    dataroot = nori.parse(File.read(filename_xml))["dataroot"]
+                                                               
+    dataroot["Skip"].map {|v|
+      vessel = {}
+      v.each do |k,v|
+        key = case k
+          when "Hovednavn" then "name"
+          when "Hovedreder" then "owner"
+          when "Tilhørighet" then "harbours"
+          when "Byggeår" then "built_year"
+          when "Byggested" then "built_where"
+          when "Bildereferanse" then "caption"
+          when "Skipsreferanse" then "sources"
+          when "Utrustning" then "description"
+          when "Historikk" then "history"
+          when "Forlist_x0020_år" then "shipwrecked_year"
+          when "Forlist_x0020_sted" then "shipwrecked_location"
+          else k.downcase
+        end
+        if key == "id"
+          clean_id = v.gsub(/[^a-zA-Z0-9æøåÆØÅ-]/u, "_")
+          if clean_id != v
+            p "#{v} => #{clean_id}"
+            #"Adm.Bra. => Adm_Bra_"
+            #"adm.san => adm_san"
+            #"earl gr => earl_gr"
+            #"fedor l => fedor_l"
+            #"fox II => fox_II"
+            #"fox III => fox_III"
+            #"Frey 1 => Frey_1"
+            #"ger ra => ger_ra"
+            #"gibr. => gibr_"
+            #"gov ru => gov_ru"
+            #"gunh. => gunh_"
+            #"gus hol => gus_hol"
+            #"hans eg => hans_eg"
+            #"herku 1 => herku_1"
+            #"herku 2 => herku_2"
+            #"Hvalr T => Hvalr_T"
+            #"jopet 1 => jopet_1"
+            #"jopet 2 => jopet_2"
+            #"jul th => jul_th"
+            #"lady j => lady_j"
+            #"mag.tø. => mag_tø_"
+            #"nils li => nils_li"
+            #"Nova Ze1 => Nova_Ze1"
+            #"ole&e1 => ole_e1"
+            #"polen Å => polen_Å"
+            #"spe & => spe__"
+            #"syv.Ham => syv_Ham"
+            #"ves møy => ves_møy"
+            #"Wil.Bar. => Wil_Bar_"
+
+          end
+          v = clean_id
+          
+        elsif key =~ /_year$/
+          v = v.to_i
+        elsif key == "harbours"
+          if v != "N. N."
+            v = v.gsub(/\.$/, "")
+          end
+          if v =~ /[\/,]/
+            v = v.split(/[\/,]/).map {|h| h.strip }
+          elsif v =~ /\sog\s/
+            v = v.split(" og ").map {|h| h.strip }
+          else
+            v=[v]
+          end
+        elsif key == "type"
+          v = v.capitalize
+        end
+        vessel[key] = v
+        
+        
+      end
+      vessel["created_by"] = "Kjell Gudmund Kjær"
+      vessel["created"] = dataroot["@generated"]
+      vessel["alpha"] = vessel["name"][0].upcase
+      vessel["collection"] = "vessel"
+      vessel
+    }
+  end
+  
+  def self.save_seed(filename_seed = "#{__dir__}/../../seed/historic/vessel/vessels.json")
+    File.open(filename_seed, "w") {|f| f.write seed.to_json}
+  end
+  
+end
+end
