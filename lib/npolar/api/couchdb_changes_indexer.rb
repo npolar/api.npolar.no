ebdba6bc7fef4239a3ad95f44195b2daf8fd9a79
Anders Bälter <anders.balter@npolar.no>
2015-03-11 17:03:33 +0100

    Updeted indexer to handle deltes

--- lib/npolar/api/couchdb_changes_indexer.rb
+++ lib/npolar/api/couchdb_changes_indexer.rb
@@ -14,53 +14,53 @@ require_relative "../validation"
 require_relative "../../service"
 
 module Npolar
-  module Api 
+  module Api
     class CouchdbChangesIndexer
-      
+
       CMD = "npolar-api-couchdb-changes-indexer"
-      
+
       PARAM = { :log => STDERR, :slice => 1000 }
-      
+
       attr_accessor :param, :log, :service
-      
+
       def initialize(service = nil)
         unless service.nil?
           @service = service
         end
       end
-      
+
       def run
 
         buffer = []
-        
+
         param[:slice] = param[:slice].to_i
-        
+
         couchdb = URI.parse(ENV["NPOLAR_API_COUCHDB"])
         couchdb.path = "/"+service.database
-    
+
         EM.run{
-        
+
           Signal.trap("INT"){EM.stop}
           Signal.trap("TERM"){EM.stop}
-        
+
           parser = Yajl::Parser.new(:symbolize_keys => true)
           parser.on_parse_complete = lambda{|json| buffer << json}
-        
+
           http = EventMachine::HttpRequest.new(
             "#{couchdb}/_changes?feed=continuous&heartbeat=1000&since=0&include_docs=true",
               :connection_timeout => 0,
               :inactivity_timeout => 0
           ).get
-          
+
           keepalive = 0
           http.stream do |chunk|
-            begin 
+            begin
               keepalive = chunk.match(/^r?\n$/) ? keepalive + 1 : 0
             rescue
               keepalive = 0
             end
             parser << chunk
-        
+
             if buffer.size == param[:slice] || keepalive >= 2
               if buffer.size > 0
                 batch = buffer.shift(param[:slice])
@@ -68,7 +68,7 @@ module Npolar
               end
             end
           end
-        
+
           http.errback {log.error "#{http.error}" ;EM.stop}
           http.callback {
             log.fatal "Stream finished"
@@ -78,17 +78,17 @@ module Npolar
         }
 
       end
-      
+
       def indexer
         log.debug "Indexing #{service.search.engine}"
         case service.search.engine
           when /Solr/
-            lambda {|changes|
-              
+            lambda { |changes|
+
               config = service.search
-              
+
               uri = config.uri? ? config.uri : config.core
-              
+
               unless uri =~ URI::REGEXP
                 uri = URI.parse(ENV["NPOLAR_API_SOLR"])
                 uri.path = uri.path.gsub(/\/$/, "") + "/" + config.core
@@ -96,105 +96,59 @@ module Npolar
               end
 
               log.debug "Solr #{uri} indexing #{changes.size} documents"
-              
+
               updates = changes.select {|c|
                 c.key?(:doc) and
                 c.key?(:id) and
                 c[:id] !~ /_design\//
-              }
-              
+              }.map {|d| d[:doc]}
+
               # Index
-              docs = updates.select {|u| u[:deleted].nil? or u[:deleted] != true }.map {|d| d[:doc]}
-             
-              if !docs.nil? and !docs.empty? 
+              docs, deletes = updates.partition {|doc| doc[:_deleted].nil? || !doc[:_deleted] }
+
+              log.debug "Docs: #{docs}"
+              log.debug "Deletes: #{deletes}"
+              if docs.any?
                 if service.model?
                   log.debug "Mapping to Solr using #{service.model}#to_solr"
                   docs.map! {|d|
                     m = Npolar::Factory.constantize(service.model).new(d)
                     m.to_solr
-                    
-                    }
+                  }
                 end
-                
-                
+
                 client = Npolar::Api::Client::JsonApiClient.new
-                responses = client.post(docs, "#{uri}/update")
-                
+                responses = client.post(docs, "#{uri}/update?commit=true")
+
                 # @todo POST errors
-                # @todo DELETE, must match /get?id=id revision, see http://wiki.apache.org/solr/RealTimeGet
                 log.debug "Responses: #{responses.map {|r| r.code}}"
               end
-            
-            }
 
-          when /Elasticsearch/
-            lambda {|changes |
-              
-              
-              changes.select! {|e| e.key?(:id)}
-              config = service.search
-              
-              url = config.uri? ? config.uri : config.url
-              
-              log.debug "#{url} indexing #{changes.size} documents"
-              
-              idx = config[:index]
-              type = config.type
-              
-              #changes.map! do |e|
-              #  operation = e[:deleted] ? :delete : :index
-              #  { operation => {:_index => idx, _type: type, _id: e[:id], data: e[:doc] } }
-              #end
-              #
-              #changes.map! do |e|
-              #  if e[:deleted]
-              #    {:delete => {:_index => search[:index], :_type => search.type, :_id => e[:id], :ignore => 404}}
-              #  else
-              #    {:index => {:_index => search[:index], :_type => search.type, :_id => e[:id], :data => e[:doc]}}
-              #  end
-              #end
-              updates = changes.select {|c|
-                c.key?(:doc) and
-                c.key?(:id) and
-                c[:id] !~ /_design\//
-              }
-              
-              # Index
-              docs = updates.select {|u| u[:deleted].nil? or u[:deleted] != true }.map {|d| d[:doc]}
-              uri = URI.parse(url)
-              uri.path = "/#{idx}/#{type}"
-              log.debug uri.to_s
-              client = Npolar::Api::Client::JsonApiClient.new(uri.to_s)
-              client.slice=1
-              docs.each do |d|
-                
-              responses = client.post(d.to_json)
+              if deletes.any?
+                data = {
+                  "delete" => deletes.map { |d| d[:_id] }
+                }
+                log.debug "Delete request: #{data}"
+                client = Npolar::Api::Client::JsonApiClient.new
+                response = client.execute(client.request("#{uri}/update?commit=true", :post, data.to_json, {}, client.options[:headers]))
+
+                # @todo POST errors
+                log.debug "Response: #{response.code}"
               end
-              #responses = client.post(docs, "#{url}")
-              
-              # @todo POST errors
-              # @todo DELETE, must match /get?id=id revision, see http://wiki.apache.org/solr/RealTimeGet
-              responses = []
-              
-              log.debug "Responses: #{responses.map {|r| r.code}}"
-              
-              #puts changes.to_json
-              #client = Elasticsearch::Client.new({url: url})
-              #client.bulk :body => changes
-              
+
             }
           else
             lambda {|changes| raise "Cannot index: #{service.search.engine}" }
         end
-        
+
       end
-      
+
       def self.param(argv)
         param = PARAM
         option_parser = OptionParser.new(argv) do |opts|
             opts.version = Npolar::Api::Client::VERSION
             opts.banner = "#{CMD} [options] /endpoint
-Options:\n"
+              Options:\n"
 
           opts.on("--slice", "-s=number", "Slice size") do |slice|
             param[:slice] = slice.to_i
@@ -203,21 +157,21 @@ Options:\n"
         option_parser.parse!
         param
       end
-      
+
       def self.run(argv=ARGV)
-        
+
         begin
-        
+
           indexer = new
-          
+
           param = param(argv)
           indexer.param = param
-          
+
           log = Logger.new(param[:log])
           indexer.log = log
-        
+
           endpoint = argv[0]
-          
+
           unless endpoint =~ URI::REGEXP
             base = URI.parse(ENV["NPOLAR_API"])
             base.path = endpoint
@@ -226,15 +180,14 @@ Options:\n"
           base = URI.parse(endpoint).host
           path = URI.parse(endpoint).path
           indexer.service = Service.services.select {|s| s.path == path }[0]
-          
+
           log.debug "#{CMD} #{endpoint} #{indexer.service.model}"
           indexer.run
           log.info "#{CMD} finished"
-          
+
         rescue => e
-          puts e
-          puts e.backtrace.join("\n")
-          exit(1)
+          log.error e
+          log.error e.backtrace.join("\n")
         end
       end
 
