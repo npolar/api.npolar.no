7ccd747fd2ebdba54657a917288541a06bf76adc
srldl <siri.uldal@gmail.com>
2016-09-05 16:17:12 +0200

    moved expedition to new schema

--- lib/expedition.rb
+++ lib/expedition.rb
@@ -4,7 +4,7 @@ class Expedition < Hashie::Mash
   include Npolar::Validation::MultiJsonSchemaValidator
 
   def schemas
-    ["http://api.npolar.no/schema/expedition"]
+    ["http://api.npolar.no/schema/expedition-1"]
   end
 
 end
