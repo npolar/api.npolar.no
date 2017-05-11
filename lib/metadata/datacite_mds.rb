c911f89ab8c118c6e268a7f9393963b971aba4dc
cnrdh <cnrdh@users.noreply.github.com>
2017-05-11 13:36:15 +0200

    Added getter for testMode

--- lib/metadata/datacite_mds.rb
+++ lib/metadata/datacite_mds.rb
@@ -4,44 +4,49 @@ require "uri"
 module Metadata
 
   class DataciteMds
-    
+
     MDS_URI = "https://mds.datacite.org"
-    
+
     @@credentials = [nil, nil]
     @@testMode = true
-    
+
     def self.credentials=c
        @@credentials=c
     end
-    
+
     def self.testMode=testMode
       @@testMode=testMode
     end
     
+    def self.testMode
+      @@testMode
+    end
+
     def self.registerDoi(doi, url, xml)
       sendMetadata(xml)
       bind(doi,url)
     end
-    
+
+    # `get': cannot access dataset which belongs to another party (RuntimeError)
     def self.getMetadata(doi)
       get("/metadata/#{doi}")
     end
-    
+
     def self.sendMetadata(xml)
       post("/metadata?testMode=#{@@testMode}", xml)
     end
-    
+
     def self.bind(doi,url)
       body = "doi=#{doi}\nurl=#{url}\n"
       post("/doi?testMode=#{@@testMode}", body, {"Content-Type"=>"text/plain"})
     end
-    
+
     def self.dois
       get("/doi").body.split(/\n/).map {|doi| doi.downcase }
     end
-    
+
     protected
-    
+
     def self.get(path,headers={})
       uri = URI.parse("#{MDS_URI}#{path}")
       http = Net::HTTP.new(uri.host, uri.port)
@@ -54,12 +59,12 @@ module Metadata
       end
       response = http.request(request)
       if response.code.to_i >= 300
-        raise response.body
+        raise "GET #{uri}\n#{response.code}\n#{response.body}"
       end
       response
     end
-    
-    
+
+
     def self.post(path, body = "", headers={"Content-Type"=>"application/xml"})
       uri = URI.parse("#{MDS_URI}#{path}")
       http = Net::HTTP.new(uri.host, uri.port)
@@ -76,9 +81,9 @@ module Metadata
         raise response.body
       end
       response
-      
+
     end
-    
-    
+
+
   end
 end
--- lib/metadata/datacite_xml.rb
+++ lib/metadata/datacite_xml.rb
@@ -1,54 +1,54 @@
 module Metadata
 
   class DataciteXml
-    
+
     DATACITE_TEST_PREFIX = "10.5072"
-      
+
     DATACITE_NAMESPACE = "http://datacite.org/schema/kernel-4"
-      
+
     NPOLAR_DOI_PREFIX = "10.21334"
 
     def self.doi(dataset,suffix=nil,prefix=DATACITE_TEST_PREFIX)
-      
+
       suffix = suffix.nil? ? dataset.id : suffix
-      
+
       if dataset.doi =~ /^10[.][0-9]+\/.+/
         dataset.doi
       else
         "#{prefix}/#{suffix}"
       end
     end
-    
+
     def self.kernel(dataset,doi)
       publicationYear = (!dataset.released.nil? and dataset.released =~ /^\d{4}-/) ? dataset.released.split("-").first : dataset.created.split("-").first
-      publisher = dataset.organisations.select {|o| o.roles.include? "publisher"}.map {|p| p.id||p.name }.join(", ")
-      creators = dataset.people.select {|p| p.roles.include? "author"}.map {|p| {
+      publisher = (dataset.organisations||[]).select {|o| o.roles.include? "publisher"}.map {|p| p.id||p.name }.join(", ")
+      creators = (dataset.people||[]).select {|p| p.roles.include? "author"}.map {|p| {
           creatorName: "#{p.last_name}, #{p.first_name}",
           givenName: p.first_name,
           familyName: p.last_name
         }
       }
-      
+
       if creators.none?
-        
+
         creators = dataset.organisations.select {|o| o.roles.include? "author"}.map {|o| {
           creatorName: "#{o.name} (#{o.id})"
         }
       }
-        
+
         if creators.none?
           raise "No authors: #{dataset.id}"
         end
       end
-      
-      
+
+
       return Nokogiri::XML::Builder.new(:encoding => "UTF-8") do | xml |
-        
+
         xml.resource(:xmlns => DATACITE_NAMESPACE,
           "xsi:schemaLocation" => "#{DATACITE_NAMESPACE} #{'http://schema.datacite.org/meta/kernel-4/metadata.xsd'}",
           "xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance") {
           xml.identifier({identifierType: "DOI"}, doi)
-          
+
           xml.creators {
             creators.each do |p|
               xml.creator {
@@ -56,34 +56,34 @@ module Metadata
                 #xml.givenName p.first_name
                 #xml.familyName p.last_name
                 #xml.affiliation p.organisation||(p.email||"").split("@").last
-                
+
               }
             end
           }
-          
+
           xml.titles {
-            xml.title({"xml:lang" => "en"}, dataset.title) 
+            xml.title({"xml:lang" => "en"}, dataset.title)
           }
-          
+
           xml.publisher publisher
           xml.publicationYear publicationYear
-          
+
           xml.dates {
             xml.date({dateType: "Available"}, dataset.released||nil)
             xml.date({dateType: "Updated"}, dataset.updated)
-            xml.date({dateType: "Created"}, dataset.created)              
+            xml.date({dateType: "Created"}, dataset.created)
           }
-          
+
           xml.resourceType({resourceTypeGeneral: "Dataset"}, "Dataset")
-          
+
           xml.alternateIdentifiers {
             xml.alternateIdentifier({alternateIdentifierType: "revision"}, "https://api.npolar.no/dataset/#{dataset.id}?rev=#{dataset._rev}")
           }
-          
+
           xml.relatedIdentifiers {
             xml.relatedIdentifier({relatedIdentifierType: "URL", relationType: "IsDocumentedBy"}, "https://data.npolar.no/dataset/#{dataset.id}")
           }
-          
+
           if dataset.licences.any? {|l| l =~ /creativecommons\.org\/licenses\/by\// }
             license="http://creativecommons.org/licenses/by/4.0/"
           elsif dataset.licences.any?
@@ -91,17 +91,17 @@ module Metadata
           else
             license=nil
           end
-          
+
           if not license.nil?
             xml.rightsList {
               xml.rights({ rightsURI: license }, license)
             }
           end
-        
+
           xml.descriptions {
             xml.description({descriptionType: "Abstract", "xml:lang" => "en"}, dataset.summary)
           }
-          
+
           xml.subjects {
             dataset.tags||[].each do |tag|
               xml.subject tag
@@ -109,8 +109,8 @@ module Metadata
           }
         }
       end
-      
+
     end
-    
+
   end
 end
