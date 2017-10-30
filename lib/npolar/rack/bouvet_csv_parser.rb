371e3b048887914d1383e6c488c090f8ac13c907
RDuX <ruben.dens@gmail.com>
2017-10-30 16:55:31 +0100

    rename TIMESTAMP to measured and add a UUID based on dateTime

--- lib/npolar/rack/bouvet_csv_parser.rb
+++ lib/npolar/rack/bouvet_csv_parser.rb
@@ -3,27 +3,27 @@ require 'csv'
 
 module Npolar
   module Rack
-   
+
     class BouvetCsvParser < Npolar::Rack::Middleware
 
       def condition?(request)
         create?(request)
       end
-      
+
       def handle(request)
         log.info "@BouvetCsvParser: parsing input"
         t0 = Time.now
         data = request.body.read
-       
-        docs = parse(data) 
+
+        docs = parse(data)
         request.env["rack.input"] = StringIO.new(docs.to_json)
         request.env['CONTENT_TYPE'] = "application/json"
-        
+
         log.info "@BouvetCsvParser: Input parsed in #{Time.now - t0}"
         pp docs
-        app.call(request.env)     
+        app.call(request.env)
       end
-      
+
       def create?(request)
         ["PUT", "POST"].include?(request.request_method)
       end
@@ -33,7 +33,7 @@ module Npolar
         docs = []
         rows = CSV::parse(data)
         if rows.length > 4
-          # TODO/FIXME: get Tor Ivan to make a proper csv header 
+          # TODO/FIXME: get Tor Ivan to make a proper csv header
           header = rows[1]
 
           rows[4, rows.length].each do |row|
@@ -45,17 +45,22 @@ module Npolar
               begin
                 doc[key] = Integer(val)
               rescue ArgumentError
-                begin 
+                begin
                   doc[key] = Float(val)
                 rescue ArgumentError
                 end
               end
 
-            end 
+            end
 
             # make the timestamp ours
-            toks = doc["TIMESTAMP"].split()
-            doc["TIMESTAMP"] = toks[0]+"T"+toks[1]+"Z"
+            toks = doc.delete("TIMESTAMP").split()
+            doc["measured"] = toks[0]+"T"+toks[1]+"Z"
+
+            # generate the sha256 digest based on the measurement time and use it as
+            # a namespaced UUID seed to prevent duplicate data from entering the database.
+            seed = Digest::SHA256.hexdigest doc["measured"]
+            doc["id"] = seed[0,8] + "-" + seed[8,4] + "-" + seed[12:4] + "-" + seed[16,4] + "-" + seed[20,12]
 
             # point to our schema
             doc["schema"] = "http://api.npolar.no/schema/weather-bouvet-1.0-rc1"
