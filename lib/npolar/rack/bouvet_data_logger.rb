be00d70448089d8c6fd7836d385b4578a2a3046e
RDuX <ruben.dens@gmail.com>
2017-11-02 11:18:52 +0100

    add parsing of station json data

--- lib/npolar/rack/bouvet_data_logger.rb
+++ lib/npolar/rack/bouvet_data_logger.rb
@@ -1,24 +1,53 @@
 module Npolar
-  module Rack
+    module Rack
 
-    class BouvetDataLogger < Npolar::Rack::Middleware
+        class BouvetDataLogger < Npolar::Rack::Middleware
 
-      def condition?(request)
-        create?(request)
-      end
+            def condition?(request)
+                create?(request)
+            end
 
-      def handle(request)
-        log.info "@BouvetDataLogger"
-        data = request.body.read
+            def handle(request)
+                log.info "@BouvetDataLogger"
 
-        request.env["rack.input"] = data
-        app.call(request.env)
-      end
+                data = request.body.read
+                docs = parse(data)
 
-      def create?(request)
-        ["PUT", "POST"].include?(request.request_method)
-      end
+                request.env["rack.input"] = docs
+                request.env["CONTENT_TYPE"] = "application/json"
 
+                app.call(request.env)
+            end
+
+            def create?(request)
+                ["PUT", "POST"].include?(request.request_method)
+            end
+
+            def parse(data)
+                docs = []
+
+                fields = data[:head][:fields]
+                data = data[:data]
+
+                # Check if there is a header for each data value
+                if fields.length == data[0][:vals].length
+                    data.with_index do |d,j|
+                        doc = {}
+                        doc[:measured] = data[:time]
+
+                        ## Generate a time based UUID using the sha256 sum as a seed
+                        seed = Digest::Sha256.hexdigest doc[:measured]
+                        doc[:id] = seed[0,8] + "-" + seed[8,4] + "-" + seed[12,4] + "-" + seed[16,4] + "-" seed[20,12]
+
+                        fields.with_index do |f,i|
+                            doc[f] = d[:vals][i]
+                        end
+                        docs[j] = doc
+                    end
+                end
+
+                docs
+            end
+        end
     end
-  end
 end
