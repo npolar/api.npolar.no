12c21cd7b3c3e92aad5c65970d4b5dd2b74997a5
RDuX <ruben.dens@gmail.com>
2021-05-27 10:22:46 +0200

    Add NansenLegacy set to the OAI provider
    - Define the NansenLegacy set key as an OAI set.

--- lib/metadata/oai_dataset_provider.rb
+++ lib/metadata/oai_dataset_provider.rb
@@ -13,25 +13,26 @@ module Metadata
         {:spec => "ipy.org", :name => "International Polar Year", :description => "Datasets from the International Polar Year (2007-2008), see http://www.ipy.org"},
         {:spec => "cryoclim.net", :name => "Cryoclim", :description => "Climate monitoring of the cryosphere, see http://cryoclim.net"},
         {:spec => "gcmd.nasa.gov", :name => "Global Change Master Directory" },
-        {:spec => "N-ICE2015", :name => "N-ICE2015 datasets" }
+        {:spec => "N-ICE2015", :name => "N-ICE2015 datasets" },
+        {:spec => "NansenLegacy", :name => "NansenLegacy datasets" }
       ]+self.topic_sets.map {|t| { spec: t, name: t.capitalize+" datasets", description: "See http://api.npolar.no/dataset/?q=&filter-topics=#{t}" } }
       sets
     end
-    
+
     def self.sets
       topic_sets + oai_sets.map {|os| os[:spec]}
     end
-    
+
     class << self
-      
+
       uri = URI.parse(ENV["NPOLAR_API_COUCHDB"]||"http://localhost:5984")
       uri.path = "/dataset"
-      
+
       client = Npolar::Api::Client::JsonApiClient.new(uri.to_s)
       client.model = ::Metadata::Dataset.new
       @@client=client
     end
-    
+
     # http://www.openarchives.org/OAI/2.0/oai-identifier.xsd#repositoryIdentifierType
     # /[a-zA-Z][a-zA-Z0-9\-]*(\.[a-zA-Z][a-zA-Z0-9\-]*)+/
 
@@ -42,14 +43,14 @@ module Metadata
     admin_email "data@npolar.no"
     source_model ::Metadata::OaiDumbCouchDbModel.new(@@client)
     extra_description  "OAI-PMH provider for Norwegian Polar Institute's datasets as DIF XML
-    
-      See http://api.npolar.no/dataset/?q= for a searchable Dataset API that delivers 
+
+      See http://api.npolar.no/dataset/?q= for a searchable Dataset API that delivers
       metadata of datasets in JSON, CSV, DIF, or ISO 19000 formats.
-      
+
       See also: http://data.npolar.no/datasets
-      
+
       For DIF XML, see: http://gcmd.gsfc.nasa.gov/add/difguide/index.html
-    
+
       http://api.npolar.no/dataset/oai supports all 6 verbs from v2 spec http://www.openarchives.org/OAI/openarchivesprotocol.html#ProtocolMessages
         * http://api.npolar.no/dataset/oai?verb=Identify
         * http://api.npolar.no/dataset/oai?verb=ListIdentifiers
@@ -57,7 +58,7 @@ module Metadata
         * http://api.npolar.no/dataset/oai?verb=ListSets [currently:#{self.sets.to_json}]
         * http://api.npolar.no/dataset/oai?verb=GetRecord&amp;metadataPrefix=dif&amp;identifier=0323b588-5023-57d1-bf98-201cd8192730
         * http://api.npolar.no/dataset/oai?verb=ListRecords&amp;metadataPrefix=dif
-      
+
       Sets
         * http://api.npolar.no/dataset/oai?verb=ListIdentifiers&amp;metadataPrefix=dif&amp;set=cryoclim.net
       Time range
@@ -68,6 +69,6 @@ module Metadata
     sample_id "3925805c-3ab1-44bd-99bb-a0b26321953f"
     deletion_support OAI::Const::Delete::PERSISTENT
     update_granularity OAI::Const::Granularity::HIGH
- 
+
   end
 end
