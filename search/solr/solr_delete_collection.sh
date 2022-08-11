3440c4c8da8ed1bb72b83c3022dd639a596d391f
RDuX <ruben.dens@gmail.com>
2022-08-11 14:03:43 +0200

    Cleanup solr related files.
    Solr hasn't been used as a search engine since the very early
    development stages. I'm assuming these files were kept around as some
    sort of documentation. Removing them since at this point they are more
    likely to confuse people rather than help them.

--- search/solr/solr_delete_collection.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#  ./solr_delete_collection.sh http://localhost:8983/solr/api mapping placename
-echo "Deleting $1 $2 $3"
-curl -kniXPOST $1/update --data-binary "<delete><query>workspace:$2 AND collection:$3</query></delete>" -H 'Content-Type:text/xml; charset=utf-8'
-curl -kniXPOST $1/update --data-binary '<commit/>' -H 'Content-Type:text/xml; charset=utf-8'
--- search/solr/solr_delete_id.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# ./solr_delete_id.sh http://dbmaster.data.npolar.no:8983/solr/api 8f80087e-8c7b-11e2-bc03-005056ad0004
-echo "Deleting id $2 at $1"
-curl -kniXPOST $1/update --data-binary "<delete><query>id:$2</query></delete>" -H 'Content-Type:text/xml; charset=utf-8'
-curl -kniXPOST $1/update --data-binary '<commit/>' -H 'Content-Type:text/xml; charset=utf-8'
